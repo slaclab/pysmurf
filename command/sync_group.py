85d377e274e22f6431c6df216770a07b1901dc2a
cryo <cryo@testbox03.stanford.edu>
2018-09-18 15:08:13 -0700

    Renaming to sync_group.py

--- command/sync_group.py
+++ command/sync_group.py
@@ -1,13 +1,17 @@
 import time
 import epics
 
+"""
+This class is written by Mitch to read PVs simultanesouly
+"""
 class SyncGroup(object):
     def __init__(self, pvs, timeout=30.0):
         self.pvnames = pvs
-        self.values  = dict()
+        self.values = dict()
         self.timeout = timeout
-        self.first   =  [True] * len(pvs)
-        self.pvs     = [epics.PV(pv, callback=self.channel_changed, auto_monitor=True) for pv in pvs]
+        self.first = [True] * len(pvs)
+        self.pvs = [epics.PV(pv, callback=self.channel_changed, 
+            auto_monitor=True) for pv in pvs]
 
     def channel_changed(self, pvname, value, *args, **kwargs):
         # don't fill on initial connection
@@ -33,6 +37,6 @@ class SyncGroup(object):
     def wait(self):
         t0 = time.time()
         while not all([n in self.values for n in self.pvnames]):
-            epics.ca.poll() # better performance using this over time.sleep()
+            epics.ca.poll()  # better performance using this over time.sleep()
             if time.time() - t0 > self.timeout:
                 raise Exception('Timeout waiting for PVs to update.')
