2b8ed80be626e658b9db2706ae5b8381c74e6a12
Kukhee Kim <khkim@slac.stanford.edu>
2018-11-07 13:58:02 -0800

    test script for shawn

--- scratch/eyy/debug/band_resp.py
+++ scratch/eyy/debug/band_resp.py
@@ -3,11 +3,11 @@ import glob
 import os
 import matplotlib.pyplot as plt
 
-fileroot = '/home/common/data/cpu-b000-hp01/cryo_data/data2/20180920/' + \
-    '1537461840/outputs'
-
+#fileroot = '/home/common/data/cpu-b000-hp01/cryo_data/data2/20180920/' + \
+#    '1537461840/outputs'
+fileroot = '/data/smurf_data/20181107/1541621474/outputs'
 files = glob.glob(os.path.join(fileroot, '*freq_full_band_resp.txt'))
-files = np.array(['/home/common/data/cpu-b000-hp01/cryo_data/data2/20180920/1537461840/outputs/1537461850_freq_full_band_resp.txt'])
+#files = np.array(['/home/common/data/cpu-b000-hp01/cryo_data/data2/20180920/1537461840/outputs/1537461850_freq_full_band_resp.txt'])
 n_files = len(files)
 freq = np.loadtxt(files[0])
 idx = np.argsort(freq)
@@ -17,22 +17,27 @@ n_pts = len(freq)
 resp = np.zeros((n_files, n_pts), dtype=complex)
 
 for i, f in enumerate(files):
+    print('loading data from {}'.format(f))
     resp[i] = (np.loadtxt(f.replace('freq', 'real')) + \
         1.j*np.loadtxt(f.replace('freq', 'imag')))[idx]
 
-resp = np.ravel(resp)
+fig, ax = plt.subplots(3,3, sharex=True, sharey=True, figsize=(12,9))
+resp_mean = np.mean(resp, axis=0)
+cm = plt.get_cmap('viridis')
+for i in np.arange(n_files):
+    y = i//3
+    x = i%3
+    ax[y,x].semilogy(freq, np.abs(resp[i]))
+    ax[y,x].set_title('Att {}'.format(i*3))
+    ax[y,x].plot(freq, np.abs(resp_mean))
 
-# fig, ax = plt.subplots(2, sharex=True)
-# resp_mean = np.mean(resp, axis=0)
-# for i in np.arange(n_files):
-#     ax[0].semilogy(freq, np.abs(resp[i]))
-#     ax[1].plot(freq, np.abs(resp[i] - resp_mean))
+plt.tight_layout()
 
-import pysmurf
-S = pysmurf.SmurfControl()
+#import pysmurf
+#S = pysmurf.SmurfControl()
 
-grad_loc = S.find_peak(freq, resp)
-fig, ax = plt.subplots(1)
-ax.plot(freq, np.abs(resp), '-bD', markevery=grad_loc)
-ax.set_yscale('log')
+#grad_loc = S.find_peak(freq, resp)
+#fig, ax = plt.subplots(1)
+#ax.plot(freq, np.abs(resp), '-bD', markevery=grad_loc)
+#ax.set_yscale('log')
 
