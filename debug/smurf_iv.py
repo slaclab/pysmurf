fixed radians --> pA conversions and got rid of hard-coded values (ari)

debug/smurf_iv.py
@@ -100,6 +100,7 @@ class SmurfIVMixin(SmurfBase):
         show_plot=False, save_plot=True, R_sh=.0029, high_current_mode=False,
         rn_accept_min = 1e-3, rn_accept_max = 1., phase_excursion_min=3.):
         """
+        phase_excursion: abs(max - min) of phase in radians
         """
         self.log('Analyzing from file: {}'.format(fn_iv_raw_data))
 
@@ -146,7 +147,7 @@ class SmurfIVMixin(SmurfBase):
 
                 ax.plot(phase)
                 ax.set_xlabel('Sample Num')
-                ax.set_ylabel('Phase [pA]')
+                ax.set_ylabel('Phase [rad.]')
 
                 ax.set_title('Band {}, Group {}, Ch {:03}'.format(band,
                     bias_group, ch))
@@ -199,9 +200,9 @@ class SmurfIVMixin(SmurfBase):
                 bins=np.logspace(np.floor(np.log10(
                     np.min(phase_excursion_list))),
                 np.ceil(np.log10(np.max(phase_excursion_list))),20))
-            plt.xlabel('phase excursion')
+            plt.xlabel('phase excursion (rad.)')
             plt.ylabel('number of channels')
-            plt.title('%s, band %i, group %i: %i with phase excursion > %.3e' % (basename,band,bias_group,len(phase_excursion_list),phase_excursion_min))
+            plt.title('%s, band %i, group %i: %i with phase excursion > %.3e rad.' % (basename,band,bias_group,len(phase_excursion_list),phase_excursion_min))
             plt.xscale('log')
             phase_hist_filename = os.path.join(plot_dir,'%s_IV_phase_excursion_hist.png' % (basename))
             plt.savefig(phase_hist_filename,bbox_inches='tight',dpi=300)
@@ -217,8 +218,8 @@ class SmurfIVMixin(SmurfBase):
         Args:
         -----
         v_bias (float array): The commanded bias in voltage. Length n_steps
-        resp (float array): The TES response. Of length n_pts (not the same 
-            as n_steps
+        resp (float array): The TES phase response in radians. Of length 
+                            n_pts (not the same as n_steps
 
         Returns:
         --------
@@ -227,6 +228,8 @@ class SmurfIVMixin(SmurfBase):
         idx (int array): 
         R_sh (float): Shunt resistance
         """
+        resp *= self.pA_per_phi0/(2.*np.pi*1e6) # convert phase to uA
+
         n_pts = len(resp)
         n_step = len(v_bias)
 
@@ -240,7 +243,7 @@ class SmurfIVMixin(SmurfBase):
         if high_current_mode:
             # high-current mode generates higher current by decreases the in-line resistance
             r_inline /= self.high_low_current_ratio
-        i_bias = 1.0E6 * v_bias / r_inline  # Total line impedance and uA
+        i_bias = 1.0E6 * v_bias / r_inline 
 
         if make_plot:
             import matplotlib.pyplot as plt

debug/smurf_noise.py
@@ -48,7 +48,7 @@ class SmurfNoiseMixin(SmurfBase):
 
         # timestamp, I, Q = self.read_stream_data(datafile)
         timestamp, phase = self.read_stream_data(datafile)
-        phase *= 1.443
+        phase *= self.pA_per_phi0/(2.*np.pi) # phase converted to pA
 
         if fs is None:
             self.log('No flux ramp freq given. Loading current flux ramp'+
@@ -72,12 +72,12 @@ class SmurfNoiseMixin(SmurfBase):
         for c, ch in enumerate(channel):
             # phase = self.iq_to_phase(I[ch], Q[ch])
 
-            # Calculate to power spectrum and convert to pA
+            # Calculate to power spectrum
             ch_idx = 512*band + ch
             # ch_idx = ch
             f, Pxx = signal.welch(phase[ch_idx], nperseg=nperseg, 
                 fs=fs, detrend=detrend)
-            Pxx = np.sqrt(Pxx) * 1.0E6
+            Pxx = np.sqrt(Pxx)
 
             good_fit = False
             try:
@@ -322,7 +322,7 @@ class SmurfNoiseMixin(SmurfBase):
         for i, (b, d) in enumerate(zip(bias, datafile)):
             # timestamp, I, Q = self.read_stream_data(d)
             timestamp, phase = self.read_stream_data(d)
-            phase *= 1.443
+            phase *= self.pA_per_phi0/(2.*np.pi) # phase converted to pA
 
             basename, _ = os.path.splitext(os.path.basename(d))
             dirname = os.path.dirname(d)
@@ -333,7 +333,7 @@ class SmurfNoiseMixin(SmurfBase):
                 # phase = self.iq_to_phase(I[ch], Q[ch]) * 1.334  # convert to uA
                 f, Pxx = signal.welch(phase[ch], nperseg=nperseg, 
                     fs=fs, detrend=detrend)
-                Pxx = np.sqrt(Pxx) * 1.0E6  # pA
+                Pxx = np.sqrt(Pxx)  # pA
                 np.savetxt(os.path.join(psd_dir, basename + 
                     '_psd_ch{:03}.txt'.format(ch)), np.array([f, Pxx]))
 
@@ -342,7 +342,7 @@ class SmurfNoiseMixin(SmurfBase):
                     ax.plot(phase[ch])
                     ax.set_title('Channel {:03}'.format(ch))
                     ax.set_xlabel(r'Time index')
-                    ax.set_ylabel(r'Phase')
+                    ax.set_ylabel(r'Phase (pA)')
                 
                     if show_plot:
                         plt.show()