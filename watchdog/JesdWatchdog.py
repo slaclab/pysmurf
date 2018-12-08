274173758b312a5320fd0f852dffa0cb258e1753
Shawn W. Henderson <shawn@slac.stanford.edu>
2018-12-07 23:39:23 -0800

    Now logs to /tmp/JesdWatchdog.log when running - not very detailed printout, but something.  Incorporated Hugo's suggestions for making PV.puts more reliable -now spawns independent threads instead of doing EPICs PV put in monitor callback, which apparently you're not supposed to do for reliability...reasons

--- watchdog/JesdWatchdog.py
+++ watchdog/JesdWatchdog.py
@@ -1,69 +1,90 @@
 #!/usr/bin/env python
 
+import logging
 import sys
 import os
 import time
 import epics
 from datetime import datetime
 
-
 class JesdWatchdog(object):
     def __init__(self, prefix):
-        self.counterPv     = epics.PV('SIOC:SMRF:ML00:AO001CNT')
-        self.counterPvProc = epics.PV('SIOC:SMRF:ML00:AO001CNT.PROC')
-        self.enabledPv     = epics.PV('SIOC:SMRF:ML00:AO001', callback=self.enableChanged, auto_monitor=True)
-        self.JesdRxValidPv = epics.PV(prefix + ':AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdRx:DataValid', callback=self.jesdValidChanged, auto_monitor=True)
-        self.JesdTxValidPv = epics.PV(prefix + ':AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdTx:DataValid', callback=self.jesdValidChanged, auto_monitor=True)
+        self.logfile = '/tmp/JesdWatchdog.log'
+        logging.basicConfig(filename=self.logfile,level=logging.ERROR)
 
-        # for recovery
-        self.PwrUpSysRef = epics.PV(prefix + ':AMCc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:LMK:PwrUpSysRef')
-        self.JesdRxEnable = epics.PV(prefix + ':AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdRx:Enable')
-        self.JesdTxEnable = epics.PV(prefix + ':AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdTx:Enable')
-        self.DAC0JesdRstN = epics.PV(prefix + ':AMCc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:DAC[0]:JesdRstN')
-        self.DAC1JesdRstN = epics.PV(prefix + ':AMCc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:DAC[1]:JesdRstN')
+        self.prefix = prefix
+        self.jesdtxreset_thread = None
+        self.jesdrxreset_thread = None
+        self.counterPv     = epics.get_pv('SIOC:SMRF:ML00:AO001CNT')
+        self.counterPvProc = epics.get_pv('SIOC:SMRF:ML00:AO001CNT.PROC')
+        self.enabledPv     = epics.get_pv('SIOC:SMRF:ML00:AO001', callback=self.enableChanged, auto_monitor=True)
+        self.JesdRxValidPv = epics.get_pv(prefix + ':AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdRx:DataValid', callback=self.jesdValidChanged, auto_monitor=True)
+        self.JesdTxValidPv = epics.get_pv(prefix + ':AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdTx:DataValid', callback=self.jesdValidChanged, auto_monitor=True)
 
         self.enable        = self.enabledPv.get()
 
     def enableChanged(self, pvname, value, *args, **kwargs):
         self.enable = value
 
+    @staticmethod
+    def jesdRXReset(prefix):
+        logging.error('[%s] ' % str(datetime.now()) + ' JesdRx went down, will attempt to recover...')
+
+        # for recovery
+        PwrUpSysRef = epics.get_pv(prefix + ':AMCc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:LMK:PwrUpSysRef')
+        JesdRxEnable = epics.get_pv(prefix + ':AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdRx:Enable')
+
+        #1. Toggle JesdRx:Enable 0x3F3 -> 0x0 -> 0x3F3
+        JesdRxEnable.put(0x0)
+        JesdRxEnable.put(0x3F3)
+        # SYSREF is the last step
+        PwrUpSysRef.put(1)
+
+    @staticmethod
+    def jesdTXReset(prefix):       
+        logging.error('[%s] ' % str(datetime.now()) + ' JesdTx went down, will attempt to recover...')
+
+        # for recovery
+        PwrUpSysRef = epics.get_pv(prefix + ':AMCc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:LMK:PwrUpSysRef')
+        JesdTxEnable = epics.get_pv(prefix + ':AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdTx:Enable')
+        DAC0JesdRstN = epics.get_pv(prefix + ':AMCc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:DAC[0]:JesdRstN')
+        DAC1JesdRstN = epics.get_pv(prefix + ':AMCc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:DAC[1]:JesdRstN')
+
+        #1. Toggle JesdRx:Enable 0x3CF -> 0x0 -> 0x3CF
+        JesdTxEnable.put(0x0)
+        JesdTxEnable.put(0x3CF)
+
+        #2. Toggle AMCcc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:DAC[0]:JesdRstN 0x1 -> 0x0 -> 0x1
+        DAC0JesdRstN.put(0x0)
+        DAC0JesdRstN.put(0x1)
+
+        #3. Toggle AMCcc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:DAC[1]:JesdRstN 0x1 -> 0x0 -> 0x1
+        DAC1JesdRstN.put(0x0)
+        DAC1JesdRstN.put(0x1)
+
+        # SYSREF is the last step
+        PwrUpSysRef.put(1)
+
     def jesdValidChanged(self, pvname, value, *args, **kwargs):
         #print('[%s] ' % str(datetime.now()) + pvname + ' changed ; value=%s'%(str(value)))
         if self.enable == 1:
-            if value == 0:
-                
+            if value == 0:              
                 # JesdRx
-                if 'JesdRx' in pvname:
-                    print('[%s] ' % str(datetime.now()) + ' JesdRx went down, will attempt to recover...')
-                    #1. Toggle JesdRx:Enable 0x3F3 -> 0x0 -> 0x3F3
-                    self.JesdRxEnable.put(0x0)
-                    self.JesdRxEnable.put(0x3F3)
+                if 'JesdRx' in pvname:                    
+                    self.jesdrxreset_thread = epics.ca.CAThread(target=self.jesdRXReset, args=(self.prefix,))
+                    self.jesdrxreset_thread.start()
 
                 # JesdTx
                 if 'JesdTx' in pvname:
-                    print('[%s] ' % str(datetime.now()) + ' JesdTx went down, will attempt to recover...')
-                    #1. Toggle JesdRx:Enable 0x3CF -> 0x0 -> 0x3CF
-                    self.JesdTxEnable.put(0x0)
-                    self.JesdTxEnable.put(0x3CF)
-                    
-                    #2. Toggle AMCcc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:DAC[0]:JesdRstN 0x1 -> 0x0 -> 0x1
-                    self.DAC0JesdRstN.put(0x0)
-                    self.DAC0JesdRstN.put(0x1)
-                    
-                    #3. Toggle AMCcc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:DAC[1]:JesdRstN 0x1 -> 0x0 -> 0x1
-                    self.DAC1JesdRstN.put(0x0)
-                    self.DAC1JesdRstN.put(0x1)
-                
-                # SYSREF is the last step for both
-                self.PwrUpSysRef.put(1)
-
+                    self.jesdtxreset_thread = epics.ca.CAThread(target=self.jesdTXReset, args=(self.prefix,))
+                    self.jesdtxreset_thread.start()
 
     def run(self):
         count  = self.counterPv.get()
         time.sleep(5)
         count1 = self.counterPv.get()
         if count != count1:
-            print('Another process incrementing the counter')
+            logging.error('Another process incrementing the counter')
             return
 
         while True:
@@ -73,10 +94,13 @@ class JesdWatchdog(object):
         return
 
 
+
+
+
 if __name__ == '__main__':
     prefix = 'test_epics'
     if len(sys.argv) > 1:
         prefix = sys.argv[1]
-        
+
     wd = JesdWatchdog(prefix)
     wd.run()
