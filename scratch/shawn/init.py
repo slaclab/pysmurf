cb551d2334084dcb3fef4889a7a82fb05ecfbe9e
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-01-24 16:47:18 -0800

    Startup scripts that initialize a pysmurf session with and without running setup (init/setup).  Configured for campus setup.  I usually run from the command line like 'ipython3 -i init.py'

---
+++ scratch/shawn/init.py
@@ -0,0 +1,3 @@
+import pysmurf
+
+S = pysmurf.SmurfControl(make_logfile=False,setup=False,epics_root='test_epics',cfg_file='/usr/local/controls/Applications/smurf/pysmurf/pysmurf/cfg_files/experiment_fp28_smurfsrv04.cfg')
---
+++ scratch/shawn/setup.py
@@ -0,0 +1,4 @@
+import pysmurf
+
+S = pysmurf.SmurfControl(make_logfile=False,setup=True,epics_root='test_epics',cfg_file='/usr/local/controls/Applications/smurf/pysmurf/pysmurf/cfg_files/experiment_fp28_smurfsrv04.cfg')
+
