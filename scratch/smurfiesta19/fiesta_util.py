423033b297883de233fd9664f023e51cecb83cf3
smurf-srv04 <cryo@smurf-srv04.stanford.edu>
2019-02-19 21:49:25 -0800

    FIESTA!

--- scratch/smurfiesta19/fiesta_util.py
+++ scratch/smurfiesta19/fiesta_util.py
@@ -22,6 +22,7 @@ def plot_most_recent_data(channels, labels = None, downsample = 1, flt=1):
     list_of_dat_files = glob.glob('/data/smurf_data/%s/*/outputs/*.dat*'%'20190215')
     latest_file = max(list_of_dat_files, key=os.path.getctime)
     datafp=latest_file
+    datafp=datafp.replace('00005','00004')
     maskfname = datafp.split('.')[0]+'_mask.txt'
 
     print('latest_file=%s'%latest_file)
