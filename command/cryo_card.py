531a31500fafa7588849687aebac054ced419508
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-03-25 10:53:57 -0700

    Trim white spaces

--- command/cryo_card.py
+++ command/cryo_card.py
@@ -15,39 +15,39 @@ def write_csv(filename, header, line):
         f.write(line+'\n')
 
 class CryoCard():
-   def __init__(self, readpv_in, writepv_in):   
+   def __init__(self, readpv_in, writepv_in):
       self.readpv = readpv_in
       self.writepv = writepv_in
-      self.relay_address = 0x2   
+      self.relay_address = 0x2
       self.hemt_bias_address = 0x3
       self.a50K_bias_address = 0x4
       self.temperature_address = 0x5
-      self.cycle_count_address = 0x6  # used for testing  
+      self.cycle_count_address = 0x6  # used for testing
       self.adc_scale = 3.3/(1024.0 * 5);
       self.temperature_scale = 1/.028; # was 100
       self.temperature_offset =.25;
       self.bias_scale = 1.0
       self.max_retries = 5;  #number of re-tries waiting for response
       self.retry = 0 # counts nubmer of retries
-      self.busy_retry = 0  # counts number of retries due to relay busy status    
- 
+      self.busy_retry = 0  # counts number of retries due to relay busy status
+
    def do_read(self, address):
       #need double write to make sure buffer is updated
-      epics.caput(self.writepv, cmd_make(1, address, 0)) 
+      epics.caput(self.writepv, cmd_make(1, address, 0))
       for self.retry in range(0, self.max_retries):
-         epics.caput(self.writepv, cmd_make(1, address, 0)) 
+         epics.caput(self.writepv, cmd_make(1, address, 0))
          data = epics.caget(self.readpv)
          addrrb = cmd_address(data)
-         if (addrrb == address):   
+         if (addrrb == address):
             return(data)
-      return(0)     
-     
+      return(0)
+
       return (epics.caget(self.readpv))
 
    def write_relays(self, relay):  # relay is the bit partern to set
-      epics.caput(self.writepv, cmd_make(0, self.relay_address, relay)) 
+      epics.caput(self.writepv, cmd_make(0, self.relay_address, relay))
       time.sleep(0.1)
-      epics.caput(self.writepv, cmd_make(0, self.relay_address, relay)) 
+      epics.caput(self.writepv, cmd_make(0, self.relay_address, relay))
 
    def read_relays(self):
       for self.busy_retry in range(0, self.max_retries):
@@ -55,7 +55,7 @@ class CryoCard():
          if ~(data & 0x80000):  # check that not moving
             return(data & 0x7FFFF)
             time.sleep(0.1) # wait for relays to move
-      return(80000) # busy flag still set   
+      return(80000) # busy flag still set
 
 
    def delatch_bit(self, bit): # bit is the pattern for the desired relay, eg 0x4 for 100
@@ -81,13 +81,13 @@ class CryoCard():
    def read_cycle_count(self):
       data = self.do_read(self.count_address)
       return( cmd_data(data))  # do we have the right addres
-      
-      
-     
+
+
+
 # low level data conversion
 
 def cmd_read(data):  # checks for a read bit set in data
-   return( (data & 0x80000000) != 0)  
+   return( (data & 0x80000000) != 0)
 
 def cmd_address(data): # returns address data
    return((data & 0x7FFF0000) >> 20)
