84426fe7600784945a0be45c262e2388e4157fbd
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-04-11 14:34:36 -0700

    Added script for initializing pysmurf dev for hb development.

---
+++ scratch/shawn/init_dev.py
@@ -0,0 +1,8 @@
+import pysmurf
+import matplotlib.pylab as plt
+import numpy as np
+
+epics_prefix = 'smurf_server'                                                                                  
+config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_noExtRef_hbOnlyBay0.cfg'
+
+S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=False,make_logfile=False) 
