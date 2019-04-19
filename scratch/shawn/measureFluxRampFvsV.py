472bfbde5e5b1b5ae9bd832192765ecb27e7dea6
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-04-19 19:14:26 +0000

    Lots of changes to allow data to be taken on a list of bands all simultaneously, for data to be taken at a range of different tone powers, and some other improvements.

--- scratch/shawn/measureFluxRampFvsV.py
+++ scratch/shawn/measureFluxRampFvsV.py
@@ -6,18 +6,20 @@ import sys
 ## instead of takedebugdata try relaunch PyRogue, then loopFilterOutputArray, which is 
 ## the integral tracking term with lmsEnable[1..3]=0
 
-S = pysmurf.SmurfControl(make_logfile=False,setup=False,epics_root='smurf_server',cfg_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_noExtRef_hbOnlyBay0.cfg')
+S = pysmurf.SmurfControl(make_logfile=False,setup=False,epics_root='smurf_server_s5',cfg_file='/home/cryo/docker/pysmurf/hb-devel-dspv2/pysmurf/cfg_files/experiment_fp29_smurfsrv03_noExtRef_hbOnlyBay0.cfg')
 
 #######
-band=2
+hbInBay0=True
+bands=[2,3]
 Npts=3
 bias=None
 wait_time=.05
 #bias_low=-0.432
 #bias_high=0.432
-bias_low=-0.325*2
-bias_high=0.325*2
-bias_step=.0015
+bias_low=-0.65
+bias_high=0.65
+Nsteps=1000
+bias_step=np.abs(bias_high-bias_low)/float(Nsteps)
 show_plot=False
 make_plot=True
 save_plot=True 
@@ -30,153 +32,102 @@ use_take_debug_data=False
 
 # Look for good channels
 if channels is None:
-    channels = S.which_on(band)
+    channels = {}
+    for band in bands:
+        channels[band] = S.which_on(band)
+print(channels[band])
 
 if bias is None:
     bias = np.arange(bias_low, bias_high, bias_step)
 
-S.set_lms_enable1(band, 0)
-S.set_lms_enable2(band, 0)
-S.set_lms_enable3(band, 0)
+# final output data dictionary
+raw_data = {}
+print(channels[band])
+bands_with_channels_on=[]
+for band in bands:
+    print(band,channels[band])
+    if len(channels[band])>0:
+        S.log('{} channels on in band {}, configuring band for simple, integral tracking'.format(len(channels[band]),band))
+        S.log('-> Setting lmsEnable[1-3] and lmsGain to 0 for band {}.'.format(band), S.LOG_USER)
+        S.set_lms_enable1(band, 0)
+        S.set_lms_enable2(band, 0)
+        S.set_lms_enable3(band, 0)
+        S.set_lms_gain(band, 0)
 
-S.log('Turning lmsGain to 0.', S.LOG_USER)
-lms_gain = S.get_lms_gain(band)
-S.set_lms_gain(band, 0)
+        raw_data[band]={}
 
-S.log('Staring to take flux ramp.', S.LOG_USER)
+        bands_with_channels_on.append(band)
 
-sys.stdout.write('\rSetting flux ramp bias low at {:4.3f} V\033[K'.format(bias_low))
-S.set_fixed_flux_ramp_bias(bias_low)
-time.sleep(wait_time)
+bands=bands_with_channels_on
 
-fs=[]
-for b in bias:
-    sys.stdout.write('\rFlux ramp bias at {:4.3f} V\033[K'.format(b))
-    sys.stdout.flush()
-    S.set_fixed_flux_ramp_bias(b)
+amplitudes=[9,10,11,12,13,14,15]
+for amplitude in amplitudes:
+
+    ### begin retune on all bands with tones
+    for band in bands:
+        S.log('Retuning at tone amplitude {}'.format(amplitude))
+        S.set_amplitude_scale_array(band,np.array(S.get_amplitude_scale_array(band)*amplitude/np.max(S.get_amplitude_scale_array(band)),dtype=int))
+        S.run_serial_gradient_descent(band)
+        S.run_serial_eta_scan(band)
+        raw_data[band][amplitude]={}
+        
+    ### end retune
+    S.log('Starting to take flux ramp with amplitude={}.'.format(amplitude), S.LOG_USER)
+
+    sys.stdout.write('\rSetting flux ramp bias low at {:4.3f} V\033[K'.format(bias_low))
+    S.set_fixed_flux_ramp_bias(bias_low)
     time.sleep(wait_time)
 
-    if use_take_debug_data:
-        f,df,sync=S.take_debug_data(band,IQstream=False,single_channel_readout=0)
-        fsampmean=np.mean(f,axis=0)
-        fs.append(fsampmean)
-    else:
-        fsamp=np.zeros(shape=(Npts,len(channels)))
-        for i in range(Npts):
-            fsamp[i,:]=S.get_loop_filter_output_array(band)[channels]
-        fsampmean=np.mean(fsamp,axis=0)
-        fs.append(fsampmean)
+    fs={}
+    for band in bands:
+        fs[band]=[]
 
-sys.stdout.write('\n')
+    for b in bias:
+        sys.stdout.write('\rFlux ramp bias at {:4.3f} V\033[K'.format(b))
+        sys.stdout.flush()
+        S.set_fixed_flux_ramp_bias(b)
+        time.sleep(wait_time)
+
+        for band in bands:
+            if use_take_debug_data:
+                f,df,sync=S.take_debug_data(band,IQstream=False,single_channel_readout=0)
+                fsampmean=np.mean(f,axis=0)
+                fs[band].append(fsampmean)
+            else:
+                fsamp=np.zeros(shape=(Npts,len(channels[band])))
+                for i in range(Npts):
+                    fsamp[i,:]=S.get_loop_filter_output_array(band)[channels[band]]
+                fsampmean=np.mean(fsamp,axis=0)
+                fs[band].append(fsampmean)
+
+    sys.stdout.write('\n')
+
+    S.log('Done taking flux ramp with amplitude={}.'.format(amplitude), S.LOG_USER)
+
+    for band in bands:
+
+        fres=[S.channel_to_freq(band, ch) for ch in channels[band]]
+        raw_data[band][amplitude]['fres']=np.array(fres) + (2e3 if hbInBay0 else 0)
+        raw_data[band][amplitude]['channels']=channels[band]
+
+        if use_take_debug_data:
+            #stack
+            fovsfr=np.dstack(fs[band])[0]
+            [sbs,sbc]=S.get_subband_centers(band)
+            fvsfr=fovsfr[channels[band]]+[sbc[np.where(np.array(sbs)==S.get_subband_from_channel(band,ch))[0]]+S.get_band_center_mhz(band) for ch in channels[band]]
+            raw_data[band][amplitude]['fvsfr']=fvsfr + (2e3 if hbInBay0 else 0)
+        else:
+            #stack
+            lfovsfr=np.dstack(fs[band])[0]
+            raw_data[band][amplitude]['lfovsfr']=lfovsfr
+            raw_data[band][amplitude]['fvsfr']=np.array([arr/4.+fres for (arr,fres) in zip(lfovsfr,fres)]) + (2e3 if hbInBay0 else 0)
+
+raw_data['bias'] = bias
 
 # done - zero and unset
 S.set_fixed_flux_ramp_bias(0)
 S.unset_fixed_flux_ramp_bias()
 
-raw_data = {}
-
-fres=[S.channel_to_freq(band, ch) for ch in channels]
-raw_data['fres']=fres
-raw_data['channels']=channels
-
-if use_take_debug_data:
-    #stack
-    fovsfr=np.dstack(fs)[0]
-    [sbs,sbc]=S.get_subband_centers(band)
-    fvsfr=fovsfr[channels]+[sbc[np.where(np.array(sbs)==S.get_subband_from_channel(band,ch))[0]]+S.get_band_center_mhz(band) for ch in channels]
-    raw_data['fvsfr']=fvsfr
-else:
-    #stack
-    lfovsfr=np.dstack(fs)[0]
-    raw_data['lfovsfr']=lfovsfr
-    raw_data['fvsfr']=np.array([arr/4.+fres for (arr,fres) in zip(lfovsfr,fres)])
-
-raw_data['bias'] = bias
-raw_data['band'] = band
-
 import os
 fn_raw_data = os.path.join('./', '%s_fr_sweep_data.npy'%(S.get_timestamp()))
 np.save(fn_raw_data, raw_data)
-
-###
-import matplotlib.pylab as plt
-# if loading from file *.item(); e.g.;
-#d=np.load('1543444743_fr_sweep_data.npy')
-#raw_data=d.item()
-def plot(chans,raw_data):
-    for ch in chans:
-        chf=raw_data['fvsfr'][np.where(raw_data['channels']==ch)[0][0]]
-        chf=chf-np.mean(chf)
-        plt.plot(raw_data['bias'],chf,label='ch%d'%ch)
-    plt.xlabel('fraction fullscale (one-sided)')
-    plt.ylabel('Foff (MHz)')
-    plt.legend()
-    plt.show()
-###
-
-sys.exit(1)
-
-plot([385],raw_data)
-
-## try to analyze
-def fourier(x, *a):
-    ret = a[3] + a[2] * np.sin(np.pi / a[1] * (x - a[0] ) )
-    for deg in range(4, len(a)):
-        ret += a[deg] * np.sin((deg+1) * np.pi / a[1] * (x - a[0]) )
-    return ret
-
-from scipy import signal
-from scipy.optimize import curve_fit
-def analyze(ch,raw_data):
-    chf=raw_data['fvsfr'][np.where(raw_data['channels']==ch)[0][0]]
-    bias=raw_data['bias']
-    
-    #peakind = signal.find_peaks_cwt(chf, np.linspace(0.05,0.4,4))
-
-    biasspan=np.max(bias)-np.min(bias)
-    fspan=np.max(chf)-np.min(chf)
-    fmean=np.mean(chf)
-    meanschf=chf-fmean
-    peakind=signal.find_peaks(meanschf,threshold=0,distance=20)[0]
-
-    Fres=S.channel_to_freq(raw_data['band'],ch)
-    if len(peakind)==0:
-        return
-    try:
-        qest=np.abs(bias[peakind[1]]-bias[peakind[0]])/2.
-        phest=np.max(np.diff(bias[peakind]))
-    except:
-        plt.plot(bias,chf,label='ch%d data'%(ch))    
-        plt.title('ch%d -- FAILED'%(ch))
-        print('ch%d FAILED!'%ch)
-        plt.legend()
-        plt.show()
-        return
-
-    Fres=S.channel_to_freq(raw_data['band'],ch)
-    print('%d\t%0.3f\t%0.4f\t%0.4f\n'%(ch,Fres,phest,fspan))
-    of=open('phest.dat','a+')
-    of.write('%d\t%0.3f\t%0.4f\n'%(ch,Fres,phest))
-    of.close()
-
-    plt.plot(bias,chf,label='ch%d data'%(ch))    
-    plt.scatter(bias[peakind],chf[peakind],label='maxes')
-
-    plt.title('ch%d  Fres=%0.3f  phest=%0.4f%%'%(ch,Fres,phest))
-
-#    # fit first harmonic
-#    p0=[qest+phest/2,qest,fspan/2,fmean]
-#    param_bounds=([0,        biasspan/20., -np.inf,-np.inf],
-#                  [2.*np.pi,     biasspan,  np.inf, np.inf])
-#    popt, pcov = curve_fit(fourier, bias, chf, p0, bounds=param_bounds)
-#
-#    plt.plot(bias,fourier(bias,*p0),'g--',label='1st harmonic guess')
-#    plt.plot(bias,fourier(bias,*popt),label='fit')
-#    
-    plt.legend()
-    plt.savefig('ch%d.png'%ch)
-
-#for ch in raw_data['channels']:                                    
-#    analyze(ch,raw_data)                                              
-#    plt.waitforbuttonpress()                                          
-#    plt.clf() 
