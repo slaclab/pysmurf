88658396e645ea931818c39ea531d8437c37b9cc
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-04-19 19:13:50 +0000

    Modified to use slot5 smurf_server for HB testing.

--- scratch/shawn/init_dev.py
+++ scratch/shawn/init_dev.py
@@ -2,7 +2,7 @@ import pysmurf
 import matplotlib.pylab as plt
 import numpy as np
 
-epics_prefix = 'smurf_server'                                                                                  
+epics_prefix = 'smurf_server_s5' 
 config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_noExtRef_hbOnlyBay0.cfg'
 
 S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=False,make_logfile=False) 
