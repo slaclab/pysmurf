a90952c9d6ae1ad83b0f19047d216f6030eec280
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-04-30 11:43:43 -0700

    Modified to take as input which slot you're running on.

--- scratch/eyy/ipython_start/init_dspv3.py
+++ scratch/eyy/ipython_start/init_dspv3.py
@@ -1,8 +1,17 @@
 import pysmurf
 # import matplotlib.pylab as plt
 import numpy as np
+import sys
+slot=int(sys.argv[1])
 
-epics_prefix = 'smurf_server_s2'
-config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_noExtRef_lbOnlyBay0.cfg'
+if slot==5:
+    epics_prefix = 'smurf_server_s5'
+    config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_noExtRef_hbOnlyBay0.cfg'
+elif slot==2:
+    epics_prefix = 'smurf_server_s2'
+    config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_noExtRef_lbOnlyBay0.cfg'
+else:
+    assert False,"There's nothing in slot %d right now!"%slot
+
 
 S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=False,make_logfile=False) 
