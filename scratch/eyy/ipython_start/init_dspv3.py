f08d149e9bdcddb3781b8b79c7e912313db38245
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-05-17 17:06:08 -0700

    Modified to point to right cfg file on smurf-srv10 where we're working on getting the PCIe interface working.

--- scratch/eyy/ipython_start/init_dspv3.py
+++ scratch/eyy/ipython_start/init_dspv3.py
@@ -1,16 +1,18 @@
 import pysmurf
 # import matplotlib.pylab as plt
+import matplotlib.pylab as plt
 import numpy as np
 import sys
 slot=int(sys.argv[1])
 
 if slot==5:
     epics_prefix = 'smurf_server_s5'
-    config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_dspv3_noExtRef_hbOnlyBay0.cfg'
+    config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_dspv3_cc02-02_hbOnlyBay0.cfg'
 elif slot==4:
     epics_prefix = 'smurf_server_s4'
     #config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_dspv3_noExtRef_lbOnlyBay0.cfg'
     config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_dspv3_cc02-02_noExtRef_lbOnlyBay0.cfg'
+    #config_file='/data/pysmurf_cfg/experiment_fp29_smurfsrv03_dspv3_cc01-02_noExtRef_lbOnlyBay0.cfg'
 else:
     assert False,"There's nothing in slot %d right now!"%slot
 
