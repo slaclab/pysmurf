960565032837f58be2b3e98f22e986b9776c33ca
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-05-29 15:32:16 -0700

    For some reason this script worked well on R440 smurf-srv10, but fails on smurf-srv03, an R330.  Traced it down to a lag between starting the pyrogue docker and when the python_s# docker starts up.  Added some (bad) handling to get around the problem in the short term.

--- scratch/shawn/scripts/shawnhammer.sh
+++ scratch/shawn/scripts/shawnhammer.sh
@@ -26,15 +26,23 @@ stop_pyrogue () {
     ./stop.sh;
 }
 
+# I hate this
+wait_for_docker () {
+    latest_docker=`docker ps -a -n 1 -q`    
+}
+
 start_slot_tmux () {
     slot_number=$1
 
+    pysmurf_docker0=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
+    
     tmux new-window -t ${tmux_session_name}:${slot_number}
     tmux rename-window -t ${tmux_session_name}:${slot_number} smurf_slot${slot_number}
     tmux send-keys -t ${tmux_session_name}:${slot_number} 'cd /home/cryo/docker/smurf/slot'${slot_number}'/current' C-m
     tmux send-keys -t ${tmux_session_name}:${slot_number} './run.sh; sleep 5; docker logs smurf_server_s'${slot_number}' -f' C-m
 
-    # might be nicer to wait for heartbeat, like smurf_server_s${slot_number}:AMCc:Time
+    # might be nicer to wait for heartbeat, like
+    # smurf_server_s${slot_number}:AMCc:Time
     sleep 2
     grep -q "Starting GUI" <(docker logs smurf_server_s${slot_number} -f)
     
@@ -45,12 +53,28 @@ start_slot_tmux () {
     sleep 1
 
     tmux send-keys -t ${tmux_session_name}:${slot_number} 'ipython3 -i scratch/eyy/ipython_start/init_dspv3.py '${slot_number} C-m
+
+    ## not the safest way to do this.  If someone else starts a
+    ## pysmurf docker, will satisfy this condition.  Not even sure why
+    ## this is even needed - why is there so much latency on
+    ## smurf-srv03 between starting a docker and it showing up in
+    ## docker ps?
+    echo "pysmurf_docker0=$pysmurf_docker0"
+    latest_pysmurf_docker=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
+    echo "latest_pysmurf_docker=$latest_pysmurf_docker"	    
+    while [ "$pysmurf_docker0" == "$latest_pysmurf_docker" ]; do
+    	latest_pysmurf_docker=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
+    	sleep 1
+	echo "latest_pysmurf_docker=$latest_pysmurf_docker"		
+    done
+    
     # after running this, can run
     # pysmurf_docker=`docker ps -n 1 -q`
     # to hex of most recently created docker.
 }
 
-# right now, real dumb.  Assumes the active window in tmux is this slot's
+# right now, real dumb.  Assumes the active window in tmux is this
+# slot's
 config_pysmurf () {
     slot_number=$1
     pysmurf_docker=$2
@@ -58,7 +82,10 @@ config_pysmurf () {
     tmux send-keys -t ${tmux_session_name}:${slot_number} 'S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=True,make_logfile=False)' C-m
     
     # wait for setup to complete
-    echo "-> Waiting for carrier setup"
+    echo "-> Waiting for carrier setup (watching pysmurf docker ${pysmurf_docker})"
+    # not clear why, but on smurf-srv03 need this wait or attempt to
+    # wait until done with setup fails.
+    sleep 2
     grep -q "Done with setup" <(docker logs $pysmurf_docker -f)
     echo "-> Carrier is configured"
 
@@ -146,7 +173,8 @@ fi
 
 slot_number=4
 start_slot_tmux ${slot_number}
-pysmurf_docker_s4=`docker ps -n 1 -q`
+
+pysmurf_docker_s4=`docker ps -a -n 1 -q`
 
 if [ "$reboot" = true ] ; then
     config_pysmurf ${slot_number} ${pysmurf_docker_s4}
@@ -175,7 +203,7 @@ if [ "$configure_hb" = true ] ; then
     ./ping_carrier.sh 10.0.3.105
 
     start_slot_tmux ${slot_number}
-    pysmurf_docker_s5=`docker ps -n 1 -q`
+    pysmurf_docker_s5=`docker ps -a -n 1 -q`
     
     if [ "$reboot" = true ] ; then
 	config_pysmurf ${slot_number} ${pysmurf_docker_s5}
