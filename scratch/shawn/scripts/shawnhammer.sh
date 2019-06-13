44b4f39f8401f3c8f2a96ea01b89df3eb5b41438
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-06-13 10:24:18 -0700

    Synching nist-xray's shawn/scripts with nist-cmb's

--- scratch/shawn/scripts/shawnhammer.sh
+++ scratch/shawn/scripts/shawnhammer.sh
@@ -2,19 +2,21 @@
 
 ctime=`date +%s`
 
-attach_at_end=false
+attach_at_end=true
 screenshot_signal_analyzer=false
-configure_pysmurf=false
+configure_pysmurf=true
 reboot=true
-configure_hb=true
 using_timing_master=true
-run_half_band_test=true
+run_half_band_test=false
 one_at_a_time=true
 write_config=false
 cpwd=$PWD
 pysmurf=/home/cryo/docker/pysmurf/dspv3
+# path can assume you're in the pysmurf directory
+pysmurf_init_script=scratch/shawn/scripts/init_nist_cmb.py
 
 crate_id=3
+# LB (in slot 4), then HB (in slot 3)
 slots_in_configure_order=(4)
 
 tmux_session_name=smurf
@@ -69,7 +71,7 @@ start_slot_tmux () {
     tmux send-keys -t ${tmux_session_name}:${slot_number} './run.sh' C-m
     sleep 1
 
-    tmux send-keys -t ${tmux_session_name}:${slot_number} 'ipython3 -i scratch/shawn/scripts/init_nist.py '${slot_number} C-m
+    tmux send-keys -t ${tmux_session_name}:${slot_number} 'ipython3 -i '${pysmurf_init_script}' '${slot_number} C-m
 
     ## not the safest way to do this.  If someone else starts a
     ## pysmurf docker, will satisfy this condition.  Not even sure why
@@ -207,7 +209,7 @@ for slot in ${slots_in_configure_order[@]}; do
     # make sure ethernet is up on carrier
     echo "-> Waiting for ethernet on carrier in slot ${slot} to come up ..."
     cd $cpwd
-    ./ping_carrier.sh 10.0.${crate_id}.$((${slots_in_configure_order[0]}+100))
+    ping_carrier 10.0.${crate_id}.$((${slots_in_configure_order[0]}+100))
 
     # may only want one pyrogue server running at a time
     if [[ ! -z "$active_slot" && "$one_at_a_time" = true ]] ; then
