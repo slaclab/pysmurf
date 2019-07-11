5f13cde4baaad554074c2995cfeabe8e589dc7db
DylanShadduck <dshadducksoccer@gmail.com>
2019-07-11 15:46:56 -0700

    Commented out anything pertaining to epics

--- hwtest/smurfhwtest.py
+++ hwtest/smurfhwtest.py
@@ -4,7 +4,9 @@
 # Have not had a chance to test any of these functions
 # I still need to run Dan's server to check some things
 
-from epics import caget, caput
+# !!! Remember to uncomment epics import when merging local and regular hwtest !!!
+
+# from epics import caget, caput
 import time
 
 
@@ -28,42 +30,45 @@ class SetupHardware:
 
 		self.hw_value = hw_value
 
-	def set_all_waveforms(self, wave_value):
+	# def set_all_waveforms(self, wave_value):
+	#
+	# 	# Don't know where to find wave_form_server
+	# 	# I need to run the gui dan showed me to get a better idea
+	# 	# Should end the string with a colon before the word Base
+	# 	wave_form_server = self.location_all
+	#
+	# 	# Need to set all four Base waveforms
+	# 	for index in range(4):
+	# 		this_waveform = wave_form_server + "Base[" + str(index) + "]:waveformselect"
+	# 		caput(this_waveform, wave_value)
 
-		# Don't know where to find wave_form_server
-		# I need to run the gui dan showed me to get a better idea
-		# Should end the string with a colon before the word Base
-		wave_form_server = self.location_all
+	# def set_all_uc_atten(self, atten_value):
+	#
+	# 	# I don't know where to find uc_atten_server
+	# 	# I need to run gui dan showed me to get a better idea
+	# 	# Should end the string with 'ATT:'
+	#
+	# 	atten_server = self.location_all
+	# 	for index in range(4):
+	# 		this_atten = atten_server + "UC[" + str(index) + "]"
+	# 		caput(this_atten, atten_value)
+	#
+	# 		# Need to pause after setting each attenuator
+	# 		time.sleep(0.1)
 
-		# Need to set all four Base waveforms
-		for index in range(4):
-			this_waveform = wave_form_server + "Base[" + str(index) + "]:waveformselect"
-			caput(this_waveform, wave_value)
+	# def set_all_dc_atten(self, atten_value):
+	#
+	# 	# I don't know where to find dc_atten_server
+	# 	# I need to run gui dan showed me to get a better idea
+	# 	# Should end the string with 'ATT:'
+	# 	atten_server = self.location_all
+	#
+	# 	for index in range(4):
+	# 		this_atten = atten_server + "DC[" + str(index) + "]"
+	# 		caput(this_atten, atten_value)
+	#
+	# 		# Need to pause after setting each attenuator
+	# 		time.sleep(0.1)
 
-	def set_all_uc_atten(self, atten_value):
-
-		# I don't know where to find uc_atten_server
-		# I need to run gui dan showed me to get a better idea
-		# Should end the string with 'ATT:'
-
-		atten_server = self.location_all
-		for index in range(4):
-			this_atten = atten_server + "UC[" + str(index) + "]"
-			caput(this_atten, atten_value)
-
-			# Need to pause after setting each attenuator
-			time.sleep(0.1)
-
-	def set_all_dc_atten(self, atten_value):
-
-		# I don't know where to find dc_atten_server
-		# I need to run gui dan showed me to get a better idea
-		# Should end the string with 'ATT:'
-		atten_server = self.location_all
-
-		for index in range(4):
-			this_atten = atten_server + "DC[" + str(index) + "]"
-			caput(this_atten, atten_value)
-
-			# Need to pause after setting each attenuator
-			time.sleep(0.1)
+all_attens = SetupHardware(0, "atten")
+print("This is the address for all attenuators: ", all_attens.location_all)
