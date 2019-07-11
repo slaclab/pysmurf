27db414ada0ecf77efda79247990a5a859b2b14b
DylanShadduck <dshadducksoccer@gmail.com>
2019-07-11 14:53:21 -0700

    Added init to SetupHardware and defined location and value variables

--- hwtest/smurfhwtest.py
+++ hwtest/smurfhwtest.py
@@ -7,14 +7,33 @@
 from epics import caget, caput
 import time
 
-class Setup:
+
+class SetupHardware:
+
+	# Takes in which hardware to set (attenuator or waveform)
+	# Sets the location of the variable chosen
+	# Saves value to set for the variable chosen
+
+	def __init__(self, hw_value, hw_to_set):
+		if hw_to_set == "atten":
+
+			# Location of attenuators missing the server name and specific attenuator exp. UC[1]
+			self.location_all = ":AMCc:FpgaTopLevel:AppTop:AppCore:MicrowaveMuxCore[0]:ATT:"
+		elif hw_to_set == "waveform":
+
+			# Location of waveform missing the server name and specific base number exp. Base[0]:waveformselect
+			self.location_all = ":AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:"
+		else:
+			print("Hardware entered is not currently supported")
+
+		self.hw_value = hw_value
 
 	def set_all_waveforms(self, wave_value):
 
 		# Don't know where to find wave_form_server
 		# I need to run the gui dan showed me to get a better idea
 		# Should end the string with a colon before the word Base
-		wave_form_server = "Location of waveform variables in Dan's server"
+		wave_form_server = self.location_all
 
 		# Need to set all four Base waveforms
 		for index in range(4):
@@ -26,7 +45,8 @@ class Setup:
 		# I don't know where to find uc_atten_server
 		# I need to run gui dan showed me to get a better idea
 		# Should end the string with 'ATT:'
-		atten_server = "Location of attenuators"
+
+		atten_server = self.location_all
 		for index in range(4):
 			this_atten = atten_server + "UC[" + str(index) + "]"
 			caput(this_atten, atten_value)
@@ -39,7 +59,7 @@ class Setup:
 		# I don't know where to find dc_atten_server
 		# I need to run gui dan showed me to get a better idea
 		# Should end the string with 'ATT:'
-		atten_server = "Location of attenuators"
+		atten_server = self.location_all
 
 		for index in range(4):
 			this_atten = atten_server + "DC[" + str(index) + "]"
