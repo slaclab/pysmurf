c3a27dd7c20c559ac3702d130fd66ee0fb00d989
DylanShadduck <dshadducksoccer@gmail.com>
2019-07-12 08:40:01 -0700

    Updated set_atten to check for valid attenuator type

--- hwtest/smurfhwtest.py
+++ hwtest/smurfhwtest.py
@@ -32,24 +32,32 @@ class SetupHardware:
 		self.hw_value = hw_value
 
 	def set_atten(self, atten_type, atten_num):
-		self.location = self.location + atten_type.upper() + "[" + str(atten_num) + "]"
 
-		# ~~ FOR SERVER INTERFACE ~~
-		# caput(self.location, self.hw_value)
-		# time.sleep(0.1)
+		atten_check = atten_type.upper()
 
-		# ~~ FOR LOCAL TESTING ~~
-		print("Variable location:", self.location)
-		print("Value to set:", self.hw_value)
+		# Checks if proper attenuator type is entered
+		if atten_check == "UC" or atten_check == "DC":
+			self.atten_location = self.location + atten_check + "[" + str(atten_num) + "]"
+
+			# ~~ FOR SERVER INTERFACE ~~
+			# caput(self.location, self.hw_value)
+			# time.sleep(0.1)
+
+			# ~~ FOR LOCAL TESTING ~~
+			print("Variable location:", self.atten_location)
+			print("Value to set:", self.hw_value)
+
+		else:
+			print("ERROR: Attenuator type not recognized")
 
 	def set_waveform(self, wave_num):
-		self.location = self.location + "Base[" + str(wave_num) + "]:waveformselect"
+		self.wave_location = self.location + "Base[" + str(wave_num) + "]:waveformselect"
 
 		# ~~ FOR SERVER TESTING ~~
 		# caput(self.location, self.hw_value)
 
 		# ~~ FOR LOCAL TESTING ~~
-		print("Variable Location:", self.location)
+		print("Variable Location:", self.wave_location)
 		print("Value to set:", self.hw_value)
 
 	def set_all_waveforms(self):
@@ -65,6 +73,23 @@ class SetupHardware:
 		for index in range(1, 5):
 			self.set_atten(atten_type, index)
 
+# Variables to use for local testing
+# __________________________________
 
-all_attens = SetupHardware(0, "atten")
-all_attens.set_all_attens("uc")
+# Testing UC attenuators
+print("\n")
+print("Testing all UC attenuators...")
+uc_attens = SetupHardware(0, "atten")
+uc_attens.set_all_attens("uc")
+
+# Testing DC attenuators
+print("\n")
+print("Testing all DC attenuators...")
+dc_attens = SetupHardware(1, "atten")
+dc_attens.set_all_attens("dc")
+
+# Testing invalid type attenuator
+print("\n")
+print("Testing incorrect attenuator type...")
+invalid_attens = SetupHardware(3, "atten")
+invalid_attens.set_atten("lc", 2)
