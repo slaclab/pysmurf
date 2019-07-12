dddd6b8ac4e02f0d7d1b03d9d1a2d22e07518939
DylanShadduck <dshadducksoccer@gmail.com>
2019-07-12 13:26:36 -0700

    Made error messages easier to understand

--- hwtest/smurfhwtest.py
+++ hwtest/smurfhwtest.py
@@ -48,7 +48,10 @@ class SetupHardware:
 			if hw_inst in range(1,5):
 				self.inst = hw_inst
 			elif hw_inst == -1:
-				self.inst = 1
+				# This is a place holder
+				# If user doesn't input an instance we will assume they want to
+				# update all instances of the hardware at the same time
+				self.inst = hw_inst
 			else:
 				print("ERROR: Unrecognized instance")
 
@@ -61,12 +64,14 @@ class SetupHardware:
 			if hw_inst in range(4):
 				self.inst = hw_inst
 			elif hw_inst == -1:
-				self.inst = 1
+				# This is a place holder
+				# If user doesn't input an instance we will assume they want to
+				# update all instances of the hardware at the same time
+				self.inst = hw_inst
 			else:
 				print("ERROR: Unrecognized instance")
 
 		else:
-			print("Hardware entered is not currently supported")
 			self.location = "LOCATION UNKNOWN"
 			self.inst = hw_inst
 
@@ -82,31 +87,60 @@ class SetupHardware:
 			print("Attenuator value has been set to default of 0")
 			value_to_set = 0
 
-		atten_location = self.location + "[" + str(self.inst) + "]"
+		if self.inst == -1:
+			# -1 is the default for init function
+			# We return an error because the user did not specify which instance
+			# to write this value to
+			print("ERROR: Attenuator instance was not specified")
 
-		# ~~ FOR SERVER INTERFACE ~~
-		# caput(atten_location, value_to_set)
-		# time.sleep(0.1)
+		elif "dans_epics" not in self.location:
+			# This is true if hardware entered is not supported
+			print("ERROR: Unrecognized hardware name")
 
-		# ~~ FOR LOCAL TESTING ~~
-		print("Variable location:", atten_location)
-		print("Value to set:", value_to_set)
+		else:
+
+			# If instance is properly defined, we are able to
+			# write a value to the attenuator variable
+			atten_location = self.location + "[" + str(self.inst) + "]"
+
+			# ~~ FOR SERVER INTERFACE ~~
+			# caput(atten_location, value_to_set)
+			# time.sleep(0.1)
+
+			# ~~ FOR LOCAL TESTING ~~
+			print("Variable location:", atten_location)
+			print("Value to set:", value_to_set)
 
 	def set_waveform(self, wave_value):
-		wave_location = self.location + "Base[" + str(self.inst) + "]:waveformselect"
 
-		if wave_value == 0 or wave_value == 1:
-			value_to_set = wave_value
+		if self.inst == -1:
+			# -1 is the default for init function
+			# We return an error because the user did not specify which instance
+			# to write this value to
+			print("ERROR: Waveform instance was not specified")
+
+		elif "dans_epics" not in self.location:
+			# This is true if hardware entered is not supported
+			print("ERROR: Unrecognized hardware name")
+
 		else:
-			print("Waveform has been set to default value of 0")
-			value_to_set = 0
 
-		# ~~ FOR SERVER TESTING ~~
-		# caput(wave_location, value_to_set)
+			# If instance is properly specified, we are able to
+			# write the value to waveform variable
+			wave_location = self.location + "[" + str(self.inst) + "]"
 
-		# ~~ FOR LOCAL TESTING ~~
-		print("Variable Location:", wave_location)
-		print("Value to set:", value_to_set)
+			if wave_value == 0 or wave_value == 1:
+				value_to_set = wave_value
+			else:
+				print("Waveform has been set to default value of 0")
+				value_to_set = 0
+
+			# ~~ FOR SERVER TESTING ~~
+			# caput(wave_location, value_to_set)
+
+			# ~~ FOR LOCAL TESTING ~~
+			print("Variable Location:", wave_location)
+			print("Value to set:", value_to_set)
 
 	def set_all_waveforms(self, wave_value):
 
@@ -166,3 +200,9 @@ print("\n")
 print("Testing set_waveform with instance 3 and value 0...")
 my_waveform = SetupHardware(hw_types[2], hw_inst=3)
 my_waveform.set_waveform(0)
+
+# Testing what happens when instance is forgotten for a single attenuator
+print("\n")
+print("Testing unspecified uc attenuator instance...")
+unknown_atten = SetupHardware(hw_types[0])
+unknown_atten.set_atten(8)
