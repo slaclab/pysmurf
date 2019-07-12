22c605cf99ba67364be653c6b49d2d483d4dcb3d
DylanShadduck <dshadducksoccer@gmail.com>
2019-07-12 09:16:52 -0700

    Can now specify attenuator value from set_atten function

--- hwtest/smurfhwtest.py
+++ hwtest/smurfhwtest.py
@@ -16,7 +16,11 @@ class SetupHardware:
 	# Sets the location of the variable chosen
 	# Saves value to set for the variable chosen
 
-	def __init__(self, hw_value, hw_to_set):
+	# From Dan's code
+	# This is a list of values that an attenuator can be set to
+	atten_values = [0, 1, 2, 4, 8, 16, 31]
+
+	def __init__(self, hw_to_set, hw_value=0):
 		if hw_to_set == "atten":
 
 			# Location of attenuators missing the server name and specific attenuator exp. UC[1]
@@ -31,7 +35,20 @@ class SetupHardware:
 
 		self.hw_value = hw_value
 
-	def set_atten(self, atten_type, atten_num):
+	def set_atten(self, atten_type, atten_num, atten_value=-1):
+
+		# This allows for user to input the attenuator value at init or here.
+		# We are checking to see if user enters a number
+		if atten_value == -1:
+			pass
+
+		else:
+			# Here we make sure user entered attenuator value is a valid value to set
+			if atten_value in SetupHardware.atten_values:
+				self.hw_value = atten_value
+			else:
+				print("Attenuator value has been set to default of 0")
+				self.hw_value = 0
 
 		atten_check = atten_type.upper()
 
@@ -79,17 +96,24 @@ class SetupHardware:
 # Testing UC attenuators
 print("\n")
 print("Testing all UC attenuators...")
-uc_attens = SetupHardware(0, "atten")
+uc_attens = SetupHardware("atten")
 uc_attens.set_all_attens("uc")
 
 # Testing DC attenuators
 print("\n")
 print("Testing all DC attenuators...")
-dc_attens = SetupHardware(1, "atten")
+dc_attens = SetupHardware("atten", 1)
 dc_attens.set_all_attens("dc")
 
 # Testing invalid type attenuator
 print("\n")
 print("Testing incorrect attenuator type...")
-invalid_attens = SetupHardware(3, "atten")
+invalid_attens = SetupHardware("atten", 3)
 invalid_attens.set_atten("lc", 2)
+
+# Testing atten_values list
+print("\n")
+print("Testing atten_values...")
+my_atten = SetupHardware("atten")
+for num in SetupHardware.atten_values:
+	my_atten.set_atten("UC", "3", num)
