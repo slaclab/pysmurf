bef7faf70d9bab7ff761572099a4c955224cdc8d
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-07-18 18:47:32 -0700

    New file, init_rflab.py for managing rf lab setup.  Got rid of shawnhammer_fullb.sh script.  Moved shawnhammer functions into a separate shawnhammerfunctions script, which is sourced at the start of shawnhammer.  Added options in shawnhammer to start atca_monitor docker and set crate fan speed to full at the very start of setup. TPG pane in shawnhammer tmux utils session now only opened if using the timing master (which is determined by the value of the using_timing_master flag).  Shelfmanager now a variable in shawnhammer. Modified some of the shawnhammer functions to exploit the new smurf docker -N slot number flag.

--- scratch/shawn/scripts/shawnhammer.sh
+++ scratch/shawn/scripts/shawnhammer.sh
@@ -1,132 +1,36 @@
 #!/bin/bash
 
+source shawnhammerfunctions
+
 ctime=`date +%s`
 
+set_crate_fans_to_full=true
 attach_at_end=true
 screenshot_signal_analyzer=false
 configure_pysmurf=true
 reboot=true
-using_timing_master=true
+using_timing_master=false
 run_half_band_test=false
 one_at_a_time=true
 write_config=false
+start_atca_monitor=true
 cpwd=$PWD
 
 pysmurf=/home/cryo/docker/pysmurf/dspv3
+shelfmanager=shm-smrf-sp01
 
 crate_id=3
 slots_in_configure_order=(4)
 
-pysmurf_init_script=scratch/shawn/scripts/init_stanford.py
+pysmurf_init_script=scratch/shawn/scripts/init_rflab.py
 
 tmux_session_name=smurf
 
-matching_dockers () {
-    # the $1 in the single quotes doesn't get replaced with the input
-    # arg ; it's the first column of the grep output
-    if [ -z "$(docker ps | grep $1 | awk '{print $1}')" ] ; then
-	return 0
-    else
-	return 1
-    fi
-}
-
-stop_pyrogue () {
-    # stop pyrogue dockers, assumes the pyrogue version you want to use
-    # has been soft linked to slotN/current
-    echo "-> Stopping slot $1 pyrogue docker";
-    cd /home/cryo/docker/smurf/slot$1/current;
-    ./stop.sh;
-}
-
-# I hate this
-wait_for_docker () {
-    latest_docker=`docker ps -a -n 1 -q`    
-}
-
-start_slot_tmux () {
-    slot_number=$1
-
-    pysmurf_docker0=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
-    
-    tmux new-window -t ${tmux_session_name}:${slot_number}
-    tmux rename-window -t ${tmux_session_name}:${slot_number} smurf_slot${slot_number}
-    tmux send-keys -t ${tmux_session_name}:${slot_number} 'cd /home/cryo/docker/smurf/slot'${slot_number}'/current' C-m
-    tmux send-keys -t ${tmux_session_name}:${slot_number} './run.sh; sleep 5; docker logs smurf_server_s'${slot_number}' -f' C-m
-
-
-    echo '-> Waiting for smurf_server_s'${slot_number}' docker to start.'
-    while [[ -z `docker ps  | grep smurf_server_s${slot_number}`  ]]; do
-	sleep 1
-    done
-    echo '-> smurf_server_s'${slot_number}' docker started.'    
-    
-    echo '-> Waiting for smurf_server_s'${slot_number}' GUI to come up.'    
+# If true, forces crate fans to full speed
+if [ "$set_crate_fans_to_full" = true ] ; then
+    ssh root@${shelfmanager} "clia minfanlevel 15; clia setfanlevel all 15"
     sleep 2
-    grep -q "Starting GUI" <(docker logs smurf_server_s${slot_number} -f)
-    
-    # start pysmurf in a split window and initialize the carrier
-    tmux split-window -v -t ${tmux_session_name}:${slot_number}
-    tmux send-keys -t ${tmux_session_name}:${slot_number} 'cd '${pysmurf} C-m
-    tmux send-keys -t ${tmux_session_name}:${slot_number} './run.sh' C-m
-    sleep 1
-
-    tmux send-keys -t ${tmux_session_name}:${slot_number} 'ipython3 -i '${pysmurf_init_script}' '${slot_number} C-m
-
-    ## not the safest way to do this.  If someone else starts a
-    ## pysmurf docker, will satisfy this condition.  Not even sure why
-    ## this is even needed - why is there so much latency on
-    ## smurf-srv03 between starting a docker and it showing up in
-    ## docker ps?
-    echo "pysmurf_docker0=$pysmurf_docker0"
-    latest_pysmurf_docker=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
-    echo "latest_pysmurf_docker=$latest_pysmurf_docker"	    
-    while [ "$pysmurf_docker0" == "$latest_pysmurf_docker" ]; do
-    	latest_pysmurf_docker=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
-    	sleep 1
-	echo "latest_pysmurf_docker=$latest_pysmurf_docker"		
-    done
-    
-    # after running this, can run
-    # pysmurf_docker=`docker ps -n 1 -q`
-    # to hex of most recently created docker.
-}
-
-# right now, real dumb.  Assumes the active window in tmux is this
-# slot's
-config_pysmurf () {
-    slot_number=$1
-    pysmurf_docker=$2
-    
-    tmux send-keys -t ${tmux_session_name}:${slot_number} 'S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=True,make_logfile=False)' C-m
-    
-    # wait for setup to complete
-    echo "-> Waiting for carrier setup (watching pysmurf docker ${pysmurf_docker})"
-    # not clear why, but on smurf-srv03 need this wait or attempt to
-    # wait until done with setup fails.
-    sleep 2
-    grep -q "Done with setup" <(docker logs $pysmurf_docker -f)
-    echo "-> Carrier is configured"
-
-    echo "-> Disable streaming (unless taking data)"
-    tmux send-keys -t ${tmux_session_name}:${slot_number} 'S.set_stream_enable(0)' C-m
-
-    # write config
-    if [ "$write_config" = true ] ; then
-	sleep 2    
-	tmux send-keys -t ${tmux_session_name}:${slot_number} 'S.set_read_all(write_log=True); S.write_config("/home/cryo/shawn/'${ctime}'_slot'${slot_number}'.yml")' C-m
-	sleep 45
-    fi
-
-    if [ "$run_half_band_test" = true ] ; then    
-	sleep 2
-	echo "-> Running half-band fill test"
-	tmux send-keys -t ${tmux_session_name}:${slot_number} 'sys.argv[1]='${ctime}'; exec(open("scratch/shawn/half_band_filling_test.py").read())' C-m    
-	grep -q "Done with half-band filling test." <(docker logs $pysmurf_docker -f)
-    fi
-    
-    sleep 1
-}
+fi
 
 # use tmux instead
 # https://www.peterdebelak.com/blog/tmux-scripting/
@@ -175,20 +79,37 @@ tmux rename-window -t ${tmux_session_name}:0 utils
 tmux send-keys -t ${tmux_session_name}:0 'cd /home/cryo/docker/utils' C-m
 tmux send-keys -t ${tmux_session_name}:0 './run.sh' C-m
 
-# display tpg log in tmux 0 with utils term
-tmux split-window -v -t ${tmux_session_name}:0
-tmux send-keys -t ${tmux_session_name}:0 'docker logs tpg_ioc -f' C-m
+if [ "$using_timing_master" = true ] ; then
+    # display tpg log in tmux 0 with utils term
+    tmux split-window -v -t ${tmux_session_name}:0
+    tmux send-keys -t ${tmux_session_name}:0 'docker logs tpg_ioc -f' C-m
+fi
+
+if [ "$start_atca_monitor" = true ] ; then
+    # stop any already running atca_monitor instances
+    matching_dockers smurf-atca-monitor
+    if [ "$?" = "1" ]; then
+	echo "-> Stopping all running atca_monitor dockers."
+	docker rm -f $(docker ps | grep atca_monitor | awk '{print $1}')
+    fi
+    # start the atca_monitor docker up in a pane of the utils session
+    tmux split-window -v -t ${tmux_session_name}:0
+    tmux send-keys -t ${tmux_session_name}:0 'cd /home/cryo/docker/atca_monitor' C-m
+    tmux send-keys -t ${tmux_session_name}:0 './run.sh' C-m
+fi
 
 # leave the utils pane selected
 tmux select-window -t utils
 tmux select-pane -t 0
+
+exit
 
 if [ "$reboot" = true ] ; then
 
     # deactivate carriers
     echo "-> Deactivating carrier(s) ${slots_in_configure_order[@]}"
     for slot in ${slots_in_configure_order[@]}; do
-	ssh root@shm-smrf-sp01 "clia deactivate board ${slot}"	
+	ssh root@${shelfmanager} "clia deactivate board ${slot}"	
     done    
     
     echo "-> Waiting 5 sec before re-activating carrier(s)"
@@ -197,7 +118,7 @@ if [ "$reboot" = true ] ; then
     # reactivate carriers
     echo "-> Re-activating carrier(s) ${slots_in_configure_order[@]}"
     for slot in ${slots_in_configure_order[@]}; do
-	ssh root@shm-smrf-sp01 "clia activate board ${slot}"	
+	ssh root@${shelfmanager} "clia activate board ${slot}"	
     done        
 fi
 
---
+++ scratch/shawn/scripts/shawnhammerfunctions.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+matching_dockers () {
+    # the $1 in the single quotes doesn't get replaced with the input
+    # arg ; it's the first column of the grep output
+    if [ -z "$(docker ps | grep $1 | awk '{print $1}')" ] ; then
+	return 0
+    else
+	return 1
+    fi
+}
+
+stop_pyrogue () {
+    # stop pyrogue dockers, assumes the pyrogue version you want to use
+    # has been soft linked to slotN/current
+    echo "-> Stopping slot $1 pyrogue docker";
+    cd /home/cryo/docker/smurf/current;
+    ./stop.sh -N $1;
+}
+
+# I hate this
+wait_for_docker () {
+    latest_docker=`docker ps -a -n 1 -q`    
+}
+
+start_slot_tmux () {
+    slot_number=$1
+
+    pysmurf_docker0=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
+    
+    tmux new-window -t ${tmux_session_name}:${slot_number}
+    tmux rename-window -t ${tmux_session_name}:${slot_number} smurf_slot${slot_number}
+    tmux send-keys -t ${tmux_session_name}:${slot_number} 'cd /home/cryo/docker/smurf/slot'${slot_number}'/current' C-m
+    tmux send-keys -t ${tmux_session_name}:${slot_number} './run.sh; sleep 5; docker logs smurf_server_s'${slot_number}' -f' C-m
+
+
+    echo '-> Waiting for smurf_server_s'${slot_number}' docker to start.'
+    while [[ -z `docker ps  | grep smurf_server_s${slot_number}`  ]]; do
+	sleep 1
+    done
+    echo '-> smurf_server_s'${slot_number}' docker started.'    
+    
+    echo '-> Waiting for smurf_server_s'${slot_number}' GUI to come up.'    
+    sleep 2
+    grep -q "Starting GUI" <(docker logs smurf_server_s${slot_number} -f)
+    
+    # start pysmurf in a split window and initialize the carrier
+    tmux split-window -v -t ${tmux_session_name}:${slot_number}
+    tmux send-keys -t ${tmux_session_name}:${slot_number} 'cd '${pysmurf} C-m
+    tmux send-keys -t ${tmux_session_name}:${slot_number} './run.sh' C-m
+    sleep 1
+
+    tmux send-keys -t ${tmux_session_name}:${slot_number} 'ipython3 -i '${pysmurf_init_script}' '${slot_number} C-m
+
+    ## not the safest way to do this.  If someone else starts a
+    ## pysmurf docker, will satisfy this condition.  Not even sure why
+    ## this is even needed - why is there so much latency on
+    ## smurf-srv03 between starting a docker and it showing up in
+    ## docker ps?
+    echo "pysmurf_docker0=$pysmurf_docker0"
+    latest_pysmurf_docker=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
+    echo "latest_pysmurf_docker=$latest_pysmurf_docker"	    
+    while [ "$pysmurf_docker0" == "$latest_pysmurf_docker" ]; do
+    	latest_pysmurf_docker=`docker ps -a | grep pysmurf | grep -v pysmurf_s${slot_number} | head -n 1 | awk '{print $1}'`
+    	sleep 1
+	echo "latest_pysmurf_docker=$latest_pysmurf_docker"		
+    done
+    
+    # after running this, can run
+    # pysmurf_docker=`docker ps -n 1 -q`
+    # to hex of most recently created docker.
+}
+
+# right now, real dumb.  Assumes the active window in tmux is this
+# slot's
+config_pysmurf () {
+    slot_number=$1
+    pysmurf_docker=$2
+    
+    tmux send-keys -t ${tmux_session_name}:${slot_number} 'S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=True,make_logfile=False)' C-m
+    
+    # wait for setup to complete
+    echo "-> Waiting for carrier setup (watching pysmurf docker ${pysmurf_docker})"
+    # not clear why, but on smurf-srv03 need this wait or attempt to
+    # wait until done with setup fails.
+    sleep 2
+    grep -q "Done with setup" <(docker logs $pysmurf_docker -f)
+    echo "-> Carrier is configured"
+
+    echo "-> Disable streaming (unless taking data)"
+    tmux send-keys -t ${tmux_session_name}:${slot_number} 'S.set_stream_enable(0)' C-m
+
+    # write config
+    if [ "$write_config" = true ] ; then
+	sleep 2    
+	tmux send-keys -t ${tmux_session_name}:${slot_number} 'S.set_read_all(write_log=True); S.write_config("/home/cryo/shawn/'${ctime}'_slot'${slot_number}'.yml")' C-m
+	sleep 45
+    fi
+
+    if [ "$run_half_band_test" = true ] ; then    
+	sleep 2
+	echo "-> Running half-band fill test"
+	tmux send-keys -t ${tmux_session_name}:${slot_number} 'sys.argv[1]='${ctime}'; exec(open("scratch/shawn/half_band_filling_test.py").read())' C-m    
+	grep -q "Done with half-band filling test." <(docker logs $pysmurf_docker -f)
+    fi
+    
+    sleep 1
+}
