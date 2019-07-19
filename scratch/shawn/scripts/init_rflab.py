90a85d1c35ef57e6ff45f53a1a1ad9db161783aa
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-07-19 13:03:36 -0700

    Only allows slots 2,3,4

--- scratch/shawn/scripts/init_rflab.py
+++ scratch/shawn/scripts/init_rflab.py
@@ -9,11 +9,8 @@ config_file_path='/data/pysmurf_cfg/'
 slot=int(sys.argv[1])
 epics_prefix = 'smurf_server_s%d'%slot
 
-if slot==4:
+if slot in [2,3,4]:
     config_file='experiment_fp29_srv03_dspv3_cc02-03_lbOnlyBay0.cfg'
-elif slot==5:
-    #config_file='experiment_fp29_srv03_dspv3_cc02-03.cfg'
-    config_file='experiment_fp29_srv03_dspv3_cc02-03_lbOnlyBay0.cfg'    
 else:
     assert False,"There isn't a SMuRF carrier in slot %d right now!"%slot
 
