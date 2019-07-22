a618861318e78f4a6acc267a19560dd1d97de963
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-07-22 16:52:32 -0700

    Simple version added (commented out for now) which would allow parallelization of shawnhammer steps for multiple simultaneous carrier initialization.

--- scratch/shawn/scripts/ping_carrier.sh
+++ scratch/shawn/scripts/ping_carrier.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+## Stripped down version for parallelization which returns exit value
+## 0 if ping fails or 1 if ping succeeds
+#timeout 0.2 ping -c 1 $1 >/dev/null && exit 1
+#exit 0
+
 printf "%s" "waiting for $1 ..."
 while ! timeout 0.2 ping -c 1 -n $1 &> /dev/null
 do
