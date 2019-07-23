f8cc0c0f3fa3228d0a589455dc2233f4def77fc3
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-07-23 16:08:56 -0700

    Simple script for putting the maximum number of tones in all the subbands of a band, evenly spaced.

---
+++ scratch/shawn/fill_band.py
@@ -0,0 +1,53 @@
+# Runlike this exec(open("band_filling_test.py").read())
+# to use the pysmurf S object you've already initialized
+import time
+import sys
+import numpy as np
+
+band=int(sys.argv[1])
+
+amplitude=12
+freq_std_khz=100
+freq_std_mhz=freq_std_khz/1000.
+one_subband_at_a_time=False
+wait_btw_subbands_sec=0.1
+
+print('filling bands %s'%str(band))
+
+n_subbands = S.get_number_sub_bands(band)
+digitizer_frequency_mhz = S.get_digitizer_frequency_mhz(band)
+subband_half_width = digitizer_frequency_mhz / (n_subbands / 2) / 2
+n_channels_per_subband=int(S.get_number_channels()/S.get_number_sub_bands())
+
+tone_spacing=subband_half_width/float(n_channels_per_subband)
+foffs=np.array([ch*tone_spacing for ch in range(n_channels_per_subband)])
+foffs-=np.max(foffs)/2.
+ 
+asa0=S.get_amplitude_scale_array(band)
+cfa0=S.get_center_frequency_array(band)
+asa=np.empty_like(asa0)
+cfa=np.empty_like(cfa0)
+cfa[:]=cfa0
+asa[:]=asa0
+
+subbands=np.arange(13,115)
+for sb in subbands:
+    chans_in_subband=S.get_channels_in_subband(band,sb)
+
+    cfa[chans_in_subband]=foffs
+    frand=np.random.normal(0,freq_std_mhz,n_channels_per_subband)
+    cfa[chans_in_subband]+=frand
+
+    #print(cfa[chans_in_subband])
+    asa[chans_in_subband]=amplitude
+
+    if one_subband_at_a_time:
+        print('-> Setting band {}, subband {}.'.format(band,sb))
+        S.set_center_frequency_array(band,cfa)
+        S.set_amplitude_scale_array(band,asa)        
+        time.sleep(wait_btw_subbands_sec)
+        #input('Press return to continue to next subband...')
+        
+if not one_subband_at_a_time:
+    S.set_center_frequency_array(band,cfa)
+    S.set_amplitude_scale_array(band,asa)
