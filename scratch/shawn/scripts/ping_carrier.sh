4fdaaeca0c46cc8a0b02a7119fb63f11ad009ac1
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-07-24 16:57:20 -0700

    Added -q option for parallel setup/init; -q makes it return 1 or 0 and not wait.

--- scratch/shawn/scripts/ping_carrier.sh
+++ scratch/shawn/scripts/ping_carrier.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-## Stripped down version for parallelization which returns exit value
-## 0 if ping fails or 1 if ping succeeds
-#timeout 0.2 ping -c 1 $1 >/dev/null && exit 1
-#exit 0
+quiet=false
 
-printf "%s" "waiting for $1 ..."
-while ! timeout 0.2 ping -c 1 -n $1 &> /dev/null
-do
-    printf "%c" "."
+# Parse options to the `pip` command
+while getopts ":q" opt; do
+  case ${opt} in
+    q )
+      quiet=true
+     ;;
+  esac
 done
-printf "\n%s\n"  "$1 is online"
+shift $((OPTIND -1))
+
+if [ "$quiet" = true ] ; then
+    # Stripped down version for parallelization which returns exit value
+    ## 0 if ping fails or 1 if ping succeeds
+    timeout 0.2 ping -c 1 $1 >/dev/null && exit 1
+    exit 0
+else 
+    printf "%s" "waiting for $1 ..."
+    while ! timeout 0.2 ping -c 1 -n $1 &> /dev/null
+    do
+	printf "%c" "."
+    done
+    printf "\n%s\n"  "$1 is online"
+fi
