39b6cd28a62b760bbd3de28f7dd2fb1f33a4aadf
DylanShadduck <dshadducksoccer@gmail.com>
2019-08-21 14:14:01 -0700

    Added documentation for all classes in ReadData. I will continue to add documentation to all other classes and functions for GetData and SetHardware.

--- hwtest/ReadData.py
+++ hwtest/ReadData.py
@@ -8,7 +8,14 @@ import matplotlib.pyplot as plt
 
 
 class ReadUnknown:
+	"""
+	This is a parent class for both ReadAdcData and ReadDacData
 
+	Args:
+		inst: should correspond to the instance or band of attenuator we are testing in main program
+	Returns:
+		self.bay: This attribute is to be used by ReadAdcData and ReadDacData for setting daqMux
+	"""
 	def __init__(self, inst):
 
 		if inst > 3:
@@ -22,7 +29,17 @@ class ReadUnknown:
 
 
 class ReadAdcData(ReadUnknown):
+	"""
+	Inherits some characteristics from ReadUnknown class
+	Reads the complex data from 2 streams after setting ADC daqMux
 
+	Inherits:
+		self.bay: this variable describes the bay that the card we are testing is in
+	Args:
+		inst: this variable is described in ReadUnknown and is not acted on by this class
+		datalength: describes the size of the buffer we are setting. Because we set datalength in 32 bit
+					words and we read stream data in 16 bit, we will see len(q_data) is twice datalength variable
+	"""
 	def __init__(self, inst, datalength):
 		super().__init__(inst)
 		self.my_daq.set_adc_daq(adcnumber=self.inst, datalength=datalength)
@@ -38,7 +55,17 @@ class ReadAdcData(ReadUnknown):
 
 
 class ReadDacData(ReadUnknown):
+	"""
+	Inherits some characteristics from ReadUnknown class
+	Reads the complex data from 2 streams after setting DAC daqMux
 
+	Inherits:
+		self.bay: this variable describes the bay that the card we are testing is in
+	Args:
+		inst: this variable is described in ReadUnknown and is not acted on by this class
+		datalength: describes the size of the buffer we are setting. Because we set datalength in 32 bit
+					words and we read stream data in 16 bit, we will see len(q_data) is twice datalength variable
+	"""
 	def __init__(self, inst, datalength):
 		super().__init__(inst)
 		self.my_daq.set_dac_daq(dacnumber=self.inst, datalength=datalength)
@@ -54,7 +81,19 @@ class ReadDacData(ReadUnknown):
 
 
 class FullBandResp:
+	"""
+	This class will take in complex data using ReadAdcData and ReadDacData
+	This data is then used in welch's (signal.welch) method for estimating frequency and magnitude of the spectral density
 
+	Args:
+		band:   indicates the band on the card that we will be testing. Band can range from 0-7 if we are
+				testing both cards or from 0-3 if we are only testing one card in bay 0.
+
+	Returns:
+		self.freq:  this is the frequency list we computed from running the cross power spectral density (signal.csd)
+		self.resp:  this is response of csd(adc, dac) / welch(dac)
+					I followed similar use of fullBandResp.m to compute self.resp in this way
+	"""
 	def __init__(self, band):
 
 		noiseselectpv = "dans_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[" + str(band) + "]:noiseSelect"
