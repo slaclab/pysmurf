95a16e8658af9b6ed624cc43ececab6a40427415
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-09-27 15:32:33 -0700

    Added full band response after setup, turning on streaming, and tracking setup.

--- scratch/shawn/thermal_test.py
+++ scratch/shawn/thermal_test.py
@@ -7,23 +7,30 @@ import epics
 pause_btw_stages=True
 pause_btw_band_fills=False
 pause_btw_eta_scans=False
-skip_setup=False
+skip_setup=True
 bands=range(8)
 
 shelfmanager='shm-smrf-sp01'
 
 #slots=[2,3,4]
-slots=[3]
+slots=[7]
+reset_rate_khz=10
+measure_full_band_response_after_setup=True
 
 wait_before_setup_min=1
-wait_after_setup_min=0.5
-wait_btw_band_fills_min=0.5
+wait_after_setup_min=1
+wait_btw_band_fills_min=1
 wait_after_band_fills_min=1
-wait_btw_eta_scans_min=0.5
+wait_btw_eta_scans_min=1
 wait_after_eta_scans_min=1
+wait_after_streaming_on_min=1
+# this wait is important because there's no easy way to know when each
+# bands' tracking setup is complete.
+wait_btw_tracking_setups_min=1
+wait_after_tracking_setups_min=1
 
-# Dumb monitoring of FPGA temperatures
-#while true; do clear; awk '{print $1" "$3}' 1563924543_hwlog.dat | tail -n 3 | sort; sleep 1; done
+# Dumb monitoring of FPGA and regulator temperatures
+#while true; do clear; awk '{print $1" "$3" "$17" "$18" "$19}' 1568751172_hwlog.dat | tail -n 3 | sort; sleep 1; done
 
 def tmux_cmd(slot_number,cmd,tmux_session_name='smurf'):
     os.system("""tmux send-keys -t {}:{} '{}' C-m""".format(tmux_session_name,slot_number,cmd))
@@ -59,6 +66,11 @@ def disable_streaming(slot_number):
     cmd="""S.set_stream_enable(0)"""
     tmux_cmd(slot_number,cmd)
 
+def enable_streaming(slot_number):
+    print("-> Disable streaming")
+    cmd="""S.set_stream_enable(1)"""
+    tmux_cmd(slot_number,cmd)    
+
 def carrier_setup(slot_number,shelfmanager='shm-smrf-sp01'):
     cmd="""S.shelf_manager=\"%s\"; S.setup()"""%(shelfmanager)
     tmux_cmd(slot_number,cmd)
@@ -71,6 +83,10 @@ def write_atca_monitor_state(slot_number,filename):
     cmd="""S.write_atca_monitor_state(\"{}\")""".format(filename)
     tmux_cmd(slot_number,cmd)        
 
+def measure_full_band_response(slot_number):
+    cmd="""exec(open("/usr/local/src/pysmurf/scratch/shawn/full_band_response.py").read())"""
+    tmux_cmd(slot_number,cmd)    
+    
 def fill_band(slot_number,band):
     cmd="""sys.argv[1]={}; exec(open("/usr/local/src/pysmurf/scratch/shawn/fill_band.py").read())""".format(band)
     tmux_cmd(slot_number,cmd)    
@@ -87,6 +103,10 @@ def add_tag_to_hardware_log(hardware_logfile,tag):
 def eta_scan_band(slot_number,band):
     cmd="""S.run_serial_eta_scan({})""".format(band)
     tmux_cmd(slot_number,cmd)
+
+def tracking_setup_band(slot_number,band,reset_rate_khz):
+    cmd="""S.tracking_setup({},reset_rate_khz={})""".format(band,reset_rate_khz)
+    tmux_cmd(slot_number,cmd)    
 
 def get_last_line_tmux(slot_number,tmux_session_name='smurf',offset=0):
     import subprocess
@@ -123,7 +143,7 @@ for slot in slots:
 if not skip_setup:
     print('-> Waiting {} min before setup.'.format(wait_before_setup_min))
     wait_before_setup_sec=wait_before_setup_min*60
-    time.sleep(wait_before_setup_sec)
+    time.sleep(wait_before_setup_sec)    
     
     # setup
     add_tag_to_hardware_log(hardware_logfile,tag='setup')
@@ -145,7 +165,17 @@ if not skip_setup:
 
     if pause_btw_stages:
         input('Press enter to continue ...')
+else:
+    print('-> Waiting {} min instead of setup.'.format(wait_before_setup_min))
+    wait_before_setup_sec=wait_before_setup_min*60
+    time.sleep(wait_before_setup_sec)        
 
+if measure_full_band_response_after_setup:
+    print('-> Checking full band response to confirm RF is properly configured.')    
+    measure_full_band_response(slot)
+    wait_for_text_in_tmux(slot,"Done running full_band_response.py.")
+    input('-> Visually check the measured full band response before continuing ...')
+    
 # fill bands, one at a time
 wait_btw_band_fills_sec=wait_btw_band_fills_min*60
 for band in bands:
@@ -189,6 +219,40 @@ time.sleep(wait_after_eta_scans_sec)
 
 if pause_btw_stages:
     input('Press enter to continue ...')
+
+#######################################################################    
+# streaming on
+print('-> Enabling streaming.'.format(wait_after_streaming_on_min))
+add_tag_to_hardware_log(hardware_logfile,tag='streaming'.format(band))        
+enable_streaming(slot)
+
+print('-> Waiting {} min after turning on streaming.'.format(wait_after_streaming_on_min))
+wait_after_streaming_on_sec=wait_after_streaming_on_min*60
+time.sleep(wait_after_streaming_on_sec)
+###### done enabling streaming
+
+if pause_btw_stages:
+    input('Press enter to continue ...')    
+
+#######################################################################    
+# tracking setup on all bands
+wait_btw_tracking_setups_sec=wait_btw_tracking_setups_min*60
+for band in bands:
+    add_tag_to_hardware_log(hardware_logfile,tag='b{}tracking_setup'.format(band))        
+    for slot in slots:
+        print('-> Running tracking setup on slot {}, band {}...'.format(slot,band))        
+        tracking_setup_band(slot,band)
+        print('-> Waiting {} min after band {} tracking setup.'.format(wait_btw_eta_scans_min,band))
+
+print('-> Tracking setup run on all bands on slot {}.'.format(slot))
+        
+print('-> Waiting {} min after tracking setups.'.format(wait_after_tracking_setups_min))
+wait_after_tracking_setups_sec=wait_after_tracking_setups_min*60
+time.sleep(wait_after_tracking_setups_sec)
+###### done running tracking setup
+
+if pause_btw_stages:
+    input('Press enter to continue ...')    
 
 # log atca and server ymls
 # only need once
