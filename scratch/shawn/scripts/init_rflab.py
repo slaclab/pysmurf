12bb8fe7768afdecc1e074b66ca215d44967dc1e
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-09-30 23:10:26 -0700

    Running single bay cfg in all slots.

--- scratch/shawn/scripts/init_rflab.py
+++ scratch/shawn/scripts/init_rflab.py
@@ -9,13 +9,7 @@ config_file_path='/data/pysmurf_cfg/'
 slot=int(sys.argv[1])
 epics_prefix = 'smurf_server_s%d'%slot
 
-if slot in [2,3,4,5,6,7]:
-    config_file='experiment_rflab_thermal_testing_201907.cfg'
-else:
-    assert False,"There isn't a SMuRF carrier in slot %d right now!"%slot
-
-
-config_file='experiment_rflab_thermal_testing_201907.cfg'
+config_file='experiment_rflab_onlyBay0.cfg'
 config_file=os.path.join(config_file_path,config_file)
 
 S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=False,make_logfile=False)
