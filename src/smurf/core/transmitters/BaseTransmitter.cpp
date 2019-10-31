9324f34e80bc71ed813803c94359aa774fae5ca1
Ryan Herbst <rherbst@slac.stanford.edu>
2019-10-30 23:59:38 -0700

    Add buffer count check

--- src/smurf/core/transmitters/BaseTransmitter.cpp
+++ src/smurf/core/transmitters/BaseTransmitter.cpp
@@ -136,6 +136,8 @@ void sct::BaseTransmitter::acceptMetaFrame(ris::FramePtr frame)
 {
     rogue::GilRelease noGil;
     ris::FrameLockPtr fLock = frame->lock();
+
+    if ( frame->bufferCount() != 1 ) return;
     
     std::string cfg(reinterpret_cast<char const*>(frame->beginRead().ptr()), frame->getPayload());
     fLock->unlock();
