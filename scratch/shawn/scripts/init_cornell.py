cd4053085107ef544c4adcd4ff923f6576d5dba0
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-11-05 12:39:03 -0800

    Checking in Cornell-specific configuration files.

---
+++ scratch/shawn/scripts/init_cornell.py
@@ -0,0 +1,17 @@
+import pysmurf
+import matplotlib.pylab as plt
+import numpy as np
+import sys
+import os
+
+config_file_path='/data/pysmurf_cfg/'
+
+slot=int(sys.argv[1])
+epics_prefix = 'smurf_server_s%d'%slot
+
+config_file='experiment_cornell.cfg'
+
+config_file=os.path.join(config_file_path,config_file) 
+
+S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=False,make_logfile=False)
+
