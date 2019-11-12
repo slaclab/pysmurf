db10d0d05b1b2ea606d275ca16b1e50d4b9b5d74
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-11-12 12:20:52 -0800

    Fix spaces

--- include/smurf/core/common/SmurfHeader.h
+++ include/smurf/core/common/SmurfHeader.h
@@ -53,34 +53,34 @@ public:
     static SmurfHeaderROPtr<T> create(ris::FramePtr frame);
     static SmurfHeaderROPtr<T> create(std::vector<uint8_t>& buffer);
 
-    const uint8_t  getVersion()                   const;  // Get protocol version
-    const uint8_t  getCrateID()                   const;  // Get ATCA crate ID
-    const uint8_t  getSlotNumber()                const;  // Get ATCA slot number
-    const uint8_t  getTimingConfiguration()       const;  // Get timing configuration
-    const uint32_t getNumberChannels()            const;  // Get number of channel in this packet
-    const int32_t  getTESBias(std::size_t index)  const;  // Get TES DAC values 16X 20 bit
-    void           copyTESBiasArrayTo(std::vector<uin8_t>& buffer) const;   // Copy the TES DAC full array to a buffer
-    const uint64_t getUnixTime()                  const;  // Get 64 bit unix time nanoseconds
-    const uint32_t getFluxRampIncrement()         const;  // Get signed 32 bit integer for increment
-    const uint32_t getFluxRampOffset()            const;  // Get signed 32 it integer for offset
-    const uint32_t getCounter0()                  const;  // Get 32 bit counter since last 1Hz marker
-    const uint32_t getCounter1()                  const;  // Get 32 bit counter since last external input
-    const uint64_t getCounter2()                  const;  // Get 64 bit timestamp
-    const uint32_t getAveragingResetBits()        const;  // Get up to 32 bits of average reset from timing system
-    const uint32_t getFrameCounter()              const;  // Get locally genreate frame counter 32 bit
-    const uint32_t getTESRelaySetting()           const;  // Get TES and flux ramp relays, 17bits in use now
-    const uint64_t getExternalTimeClock()         const;  // Get Syncword from mce for mce based systems (40 bit including header)
-    const uint8_t  getControlField()              const;  // Get control field word
-    const bool     getClearAverageBit()           const;  // Get control field's clear average and unwrap bit (bit 0)
-    const bool     getDisableStreamBit()          const;  // Get control field's disable stream to MCE bit (bit 1)
-    const bool     getDisableFileWriteBit()       const;  // Get control field's disable file write (bit 2)
-    const bool     getReadConfigEachCycleBit()    const;  // Get control field's set to read configuration file each cycle bit (bit 3)
-    const uint8_t  getTestMode()                  const;  // Get control field's test mode (bits 4-7)
-    const uint8_t  getTestParameters()            const;  // Get test parameters
-    const uint16_t getNumberRows()                const;  // Get MCE header value (max 255) (defaluts to 33 if 0)
-    const uint16_t getNumberRowsReported()        const;  // Get MCE header value (defaults to numb rows if 0)
-    const uint16_t getRowLength()                 const;  // Get MCE header value
-    const uint16_t getDataRate()                  const;  // Get MCE header value
+    const uint8_t  getVersion()                   const;                  // Get protocol version
+    const uint8_t  getCrateID()                   const;                  // Get ATCA crate ID
+    const uint8_t  getSlotNumber()                const;                  // Get ATCA slot number
+    const uint8_t  getTimingConfiguration()       const;                  // Get timing configuration
+    const uint32_t getNumberChannels()            const;                  // Get number of channel in this packet
+    const int32_t  getTESBias(std::size_t index)  const;                  // Get TES DAC values 16X 20 bit
+    void           copyTESBiasArrayTo(std::vector<uin8_t>& buffer) const; // Copy the TES DAC full array to a buffer
+    const uint64_t getUnixTime()                  const;                  // Get 64 bit unix time nanoseconds
+    const uint32_t getFluxRampIncrement()         const;                  // Get signed 32 bit integer for increment
+    const uint32_t getFluxRampOffset()            const;                  // Get signed 32 it integer for offset
+    const uint32_t getCounter0()                  const;                  // Get 32 bit counter since last 1Hz marker
+    const uint32_t getCounter1()                  const;                  // Get 32 bit counter since last external input
+    const uint64_t getCounter2()                  const;                  // Get 64 bit timestamp
+    const uint32_t getAveragingResetBits()        const;                  // Get up to 32 bits of average reset from timing system
+    const uint32_t getFrameCounter()              const;                  // Get locally genreate frame counter 32 bit
+    const uint32_t getTESRelaySetting()           const;                  // Get TES and flux ramp relays, 17bits in use now
+    const uint64_t getExternalTimeClock()         const;                  // Get Syncword from mce for mce based systems (40 bit including header)
+    const uint8_t  getControlField()              const;                  // Get control field word
+    const bool     getClearAverageBit()           const;                  // Get control field's clear average and unwrap bit (bit 0)
+    const bool     getDisableStreamBit()          const;                  // Get control field's disable stream to MCE bit (bit 1)
+    const bool     getDisableFileWriteBit()       const;                  // Get control field's disable file write (bit 2)
+    const bool     getReadConfigEachCycleBit()    const;                  // Get control field's set to read configuration file each cycle bit (bit 3)
+    const uint8_t  getTestMode()                  const;                  // Get control field's test mode (bits 4-7)
+    const uint8_t  getTestParameters()            const;                  // Get test parameters
+    const uint16_t getNumberRows()                const;                  // Get MCE header value (max 255) (defaluts to 33 if 0)
+    const uint16_t getNumberRowsReported()        const;                  // Get MCE header value (defaults to numb rows if 0)
+    const uint16_t getRowLength()                 const;                  // Get MCE header value
+    const uint16_t getDataRate()                  const;                  // Get MCE header value
 
     // The size of the smurf header (in bytes)
     // It has public access.
@@ -150,34 +150,34 @@ public:
     static SmurfHeaderPtr<T> create(ris::FramePtr frame);
     static SmurfHeaderPtr<T> create(std::vector<uint8_t>& buffer);
 
-    void setVersion(uint8_t value) const;                     // Set protocol version
-    void setCrateID(uint8_t value) const;                     // Set ATCA crate ID
-    void setSlotNumber(uint8_t value) const;                  // Set ATCA slot number
-    void setTimingConfiguration(uint8_t value) const;         // Set timing configuration
-    void setNumberChannels(uint32_t value) const;             // Set number of channel in this packet
-    void setTESBias(std::size_t index, int32_t value) const;  // Set TES DAC values 16X 20 bit
-    void copyTESBiasArrayFrom(const std::vector<uint8_t>& buffer) const;   // Copy the TES DAC full array from a buffer
-    void setUnixTime(uint64_t value) const;                   // Set 64 bit unix time nanoseconds
-    void setFluxRampIncrement(uint32_t value) const;          // Set signed 32 bit integer for increment
-    void setFluxRampOffset(uint32_t value) const;             // Set signed 32 it integer for offset
-    void setCounter0(uint32_t value) const;                   // Set 32 bit counter since last 1Hz marker
-    void setCounter1(uint32_t value) const;                   // Set 32 bit counter since last external input
-    void setCounter2(uint64_t value) const;                   // Set 64 bit timestamp
-    void setAveragingResetBits(uint32_t value) const;         // Set up to 32 bits of average reset from timing system
-    void setFrameCounter(uint32_t value) const;               // Set locally genreate frame counter 32 bit
-    void setTESRelaySetting(uint32_t value) const;            // Set TES and flux ramp relays, 17bits in use now
-    void setExternalTimeClock(uint64_t value) const;          // Set Syncword from mce for mce based systems (40 bit including header)
-    void setControlField(uint8_t value) const;                // Set control field word
-    void setClearAverageBit(bool value) const;                // Set control field's clear average and unwrap bit (bit 0)
-    void setDisableStreamBit(bool value) const;               // Set control field's disable stream to MCE bit (bit 1)
-    void setDisableFileWriteBit(bool value) const;            // Set control field's disable file write (bit 2)
-    void setReadConfigEachCycleBit(bool value) const;         // Set control field's set to read configuration file each cycle bit (bit 3)
-    void setTestMode(uint8_t value) const;                    // Set control field's test mode (bits 4-7)
-    void setTestParameters(uint8_t value) const;              // Set test parameters
-    void setNumberRows(uint16_t value) const;                 // Set MCE header value (max 255) (defaluts to 33 if 0)
-    void setNumberRowsReported(uint16_t value) const;         // Set MCE header value (defaults to numb rows if 0)
-    void setRowLength(uint16_t value) const;                  // Set MCE header value
-    void setDataRate(uint16_t value) const;                   // Set MCE header value
+    void setVersion(uint8_t value) const;                                // Set protocol version
+    void setCrateID(uint8_t value) const;                                // Set ATCA crate ID
+    void setSlotNumber(uint8_t value) const;                             // Set ATCA slot number
+    void setTimingConfiguration(uint8_t value) const;                    // Set timing configuration
+    void setNumberChannels(uint32_t value) const;                        // Set number of channel in this packet
+    void setTESBias(std::size_t index, int32_t value) const;             // Set TES DAC values 16X 20 bit
+    void copyTESBiasArrayFrom(const std::vector<uint8_t>& buffer) const; // Copy the TES DAC full array from a buffer
+    void setUnixTime(uint64_t value) const;                              // Set 64 bit unix time nanoseconds
+    void setFluxRampIncrement(uint32_t value) const;                     // Set signed 32 bit integer for increment
+    void setFluxRampOffset(uint32_t value) const;                        // Set signed 32 it integer for offset
+    void setCounter0(uint32_t value) const;                              // Set 32 bit counter since last 1Hz marker
+    void setCounter1(uint32_t value) const;                              // Set 32 bit counter since last external input
+    void setCounter2(uint64_t value) const;                              // Set 64 bit timestamp
+    void setAveragingResetBits(uint32_t value) const;                    // Set up to 32 bits of average reset from timing system
+    void setFrameCounter(uint32_t value) const;                          // Set locally genreate frame counter 32 bit
+    void setTESRelaySetting(uint32_t value) const;                       // Set TES and flux ramp relays, 17bits in use now
+    void setExternalTimeClock(uint64_t value) const;                     // Set Syncword from mce for mce based systems (40 bit including header)
+    void setControlField(uint8_t value) const;                           // Set control field word
+    void setClearAverageBit(bool value) const;                           // Set control field's clear average and unwrap bit (bit 0)
+    void setDisableStreamBit(bool value) const;                          // Set control field's disable stream to MCE bit (bit 1)
+    void setDisableFileWriteBit(bool value) const;                       // Set control field's disable file write (bit 2)
+    void setReadConfigEachCycleBit(bool value) const;                    // Set control field's set to read configuration file each cycle bit (bit 3)
+    void setTestMode(uint8_t value) const;                               // Set control field's test mode (bits 4-7)
+    void setTestParameters(uint8_t value) const;                         // Set test parameters
+    void setNumberRows(uint16_t value) const;                            // Set MCE header value (max 255) (defaluts to 33 if 0)
+    void setNumberRowsReported(uint16_t value) const;                    // Set MCE header value (defaults to numb rows if 0)
+    void setRowLength(uint16_t value) const;                             // Set MCE header value
+    void setDataRate(uint16_t value) const;                              // Set MCE header value
 
 private:
     // Prevent construction using the default or copy constructor.
