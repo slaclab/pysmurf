f104f84d0a750e0cbbd62432852c0a50dca24cec
Mitch D'Ewart <mdewart@slac.stanford.edu>
2020-02-07 10:46:54 -0800

    Update read_adc/read_dac to also show timeseries with ylim set to +/- full scale

--- python/pysmurf/client/util/smurf_util.py
+++ python/pysmurf/client/util/smurf_util.py
@@ -999,23 +999,31 @@ class SmurfUtilMixin(SmurfBase):
             digitizer_frequency_mhz = self.get_digitizer_frequency_mhz()
             f, p_adc = signal.welch(dat, fs=digitizer_frequency_mhz,
                 nperseg=data_length/2, return_onesided=False, detrend=False)
-            f_plot = f / 1.0E6
+            f_plot = f
 
             idx = np.argsort(f)
             f_plot = f_plot[idx]
             p_adc = p_adc[idx]
 
-            fig = plt.figure(figsize=(9,4.5))
-            ax=plt.gca()
+            fig = plt.figure(figsize=(9,9))
+            ax1 = plt.subplot(211)
+            ax1.plot(np.real(dat), label='I')
+            ax1.plot(np.imag(dat), label='Q')
+            ax1.set_xlabel('Sample number')
+            ax1.set_ylabel('Raw counts')
+            ax1.set_title(f'{timestamp} Timeseries')
+            ax1.legend()
+            ax1.set_ylim((-2**15, 2**15))
+            ax2 = plt.subplot(212)
+            ax2.plot(f_plot, 10*np.log10(p_adc))
+            ax2.set_ylabel('ADC{}'.format(band))
+            ax2.set_xlabel('Frequency [MHz]')
+            ax2.set_title(f'{timestamp} Spectrum')
+            plt.grid(which='both')
             if plot_ylimits[0] is not None:
                 plt.ylim(plot_ylimits[0],plt.ylim()[1])
             if plot_ylimits[1] is not None:
                 plt.ylim(plt.ylim()[0],plot_ylimits[1])
-            ax.set_ylabel('ADC{}'.format(band))
-            ax.set_xlabel('Frequency [MHz]')
-            ax.set_title(timestamp)
-            ax.semilogy(f_plot, p_adc)
-            plt.grid()
 
             if save_plot:
                 plot_fn = '{}/{}_adc{}.png'.format(self.plot_dir,timestamp,band)
@@ -1086,23 +1094,31 @@ class SmurfUtilMixin(SmurfBase):
             digitizer_frequency_mhz = self.get_digitizer_frequency_mhz()
             f, p_dac = signal.welch(dat, fs=digitizer_frequency_mhz,
                 nperseg=data_length/2, return_onesided=False,detrend=False)
-            f_plot = f / 1.0E6
+            f_plot = f
 
             idx = np.argsort(f)
             f_plot = f_plot[idx]
             p_dac = p_dac[idx]
 
-            fig = plt.figure(figsize=(9,4.5))
-            ax=plt.gca()
+            fig = plt.figure(figsize=(9,9))
+            ax1 = plt.subplot(211)
+            ax1.plot(np.real(dat), label='I')
+            ax1.plot(np.imag(dat), label='Q')
+            ax1.set_xlabel('Sample number')
+            ax1.set_ylabel('Raw counts')
+            ax1.set_title(f'{timestamp} Timeseries')
+            ax1.legend()
+            ax1.set_ylim((-2**15, 2**15))
+            ax2 = plt.subplot(212)
+            ax2.plot(f_plot, 10*np.log10(p_dac))
+            ax2.set_ylabel('ADC{}'.format(band))
+            ax2.set_xlabel('Frequency [MHz]')
+            ax2.set_title(f'{timestamp} Spectrum')
+            plt.grid(which='both')
             if plot_ylimits[0] is not None:
                 plt.ylim(plot_ylimits[0],plt.ylim()[1])
             if plot_ylimits[1] is not None:
                 plt.ylim(plt.ylim()[0],plot_ylimits[1])
-            ax.set_ylabel('DAC{}'.format(band))
-            ax.set_xlabel('Frequency [MHz]')
-            ax.set_title(timestamp)
-            ax.semilogy(f_plot, p_dac)
-            plt.grid()
 
             if save_plot:
                 plot_fn = '{}/{}_dac{}.png'.format(self.plot_dir,timestamp,band)
