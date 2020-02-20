b189f54f322cff75c1c3d426a1d160c44ca039f3
Shawn W. Henderson <shawn@slac.stanford.edu>
2020-02-19 21:21:21 -0800

    Checking in updated rflab init script.

--- scratch/shawn/scripts/init_rflab.py
+++ scratch/shawn/scripts/init_rflab.py
@@ -1,6 +1,5 @@
 import os
 import pysmurf
-import matplotlib.pylab as plt
 import numpy as np
 import sys
 
@@ -9,8 +8,8 @@ config_file_path='/data/pysmurf_cfg/'
 slot=int(sys.argv[1])
 epics_prefix = 'smurf_server_s%d'%slot
 
-config_file='experiment_rflab_onlyBay0.cfg'
-#config_file='experiment_rflab_thermal_testing_201907.cfg'
+#config_file='experiment_rflab_onlyBay0.cfg'
+config_file='experiment_rflab_thermal_testing_201907.cfg'
 config_file=os.path.join(config_file_path,config_file)
 
 S = pysmurf.SmurfControl(epics_root=epics_prefix,cfg_file=config_file,setup=False,make_logfile=False)
