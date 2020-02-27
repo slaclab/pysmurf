eccf6eda74be9c1237c8919b61d4358423238214
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-02-27 15:31:54 -0800

    Move the definition of __version__ to pysmurf.__init__.py

--- python/pysmurf/__init__.py
+++ python/pysmurf/__init__.py
@@ -16,3 +16,7 @@
 # copied, modified, propagated, or distributed except according to the terms
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
