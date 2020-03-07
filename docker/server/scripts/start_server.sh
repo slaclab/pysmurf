3f15ff20da42689a6f7083b3370270a05aaac8bc
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-03-06 18:14:59 -0800

    Auto-detect the type of carrier and pass the auto generated startup argument to the pyrogue server. Also, add a flag to disable the auto-detection feature.

--- docker/server/scripts/start_server.sh
+++ docker/server/scripts/start_server.sh
@@ -18,7 +18,8 @@ usage()
     echo "    -N|--slot         <slot_number>       : ATCA crate slot number. Must be used with -S."
     echo "    -a|--addr         <FPGA_IP>           : FPGA IP address. If defined, -S and -N are ignored."
     echo "    -c|--comm-type    <comm_type>         : Communication type ('eth' or 'pcie'). Default is 'eth'."
-    echo "    -D|--no-check-fw                      : Disabled FPGA version checking."
+    echo "    -D|--no-check-fw                      : Disable FPGA version checking."
+    echo "    -E|--disable-hw-detect                : Disable hardware type auto detection."
     echo "    -H|--hard-boot                        : Do a hard boot: reboot the FPGA and load default configuration."
     echo "    -h|--help                             : Show this message."
     echo "    <pyrogue_server_args> are passed to the SMuRF pyrogue server. "
@@ -29,6 +30,11 @@ usage()
     echo "The script will by default check if the firmware githash read from the FPGA via IPMI is the same of the found in the MCS file name."
     echo "If they don't match, then the MCS file will be loaded into the FPGA. If this happens, the FPGA will be rebooted."
     echo "This checking can be disabled with -D. The checking will also be disabled if -a is used instead of -S and -N. If "
+    echo
+    echo "The script will try to auto-detect the type of hardware, and automatically generate server startup arguments based on the hardware type."
+    echo "Currently, this script only detects the type of carrier board, and uses the '--enable-em22xx' option when the carrier is a Gen2, version >= C03."
+    echo "This hardware auto-detection can be disabled using the option '-E|--disable-hw-detect'. The user should provided the appropriate startup arguments"
+    echo "based on the hardware type."
     echo
     echo "The script will look for a zip file under '${fw_top_dir}'. If found, it will be passed with the argument -z to the next startup script."
     echo "If not zip file is found, the script will then look for a local checked out repository in the same location; If found, the python directories"
@@ -59,6 +65,9 @@ case ${key} in
     ;;
     -D|--no-check-fw)
     no_check_fw=1
+    ;;
+    -E|--disable-hw-detect)
+    disable_hw_detect=1
     ;;
     -a|--addr)
     fpga_ip="$2"
@@ -101,6 +110,14 @@ checkFW
 
 # Do a hard boot, if requested
 hardBoot
+
+# Auto-detect hardware type
+## Detect type of carrier, and get specific server startup arguments
+## for each specific type
+detect_carrier_board carrier_args
+
+# Add the carrier type specific argument to the list of argument
+args+=" ${carrier_args}"
 
 # Call the appropriate server startup script depending on the communication type
 if [ ${comm_type} == 'eth' ]; then
