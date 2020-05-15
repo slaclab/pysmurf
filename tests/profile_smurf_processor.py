4038f069c3c1598d3dabdaa9004922c282038fea
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-05-15 14:00:05 -0700

    Increase the simulated frame rate, so speed up the test

--- tests/profile_smurf_processor.py
+++ tests/profile_smurf_processor.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
 
     with LocalRoot() as root:
         # Start the generation of frames
-        root.StreamDataSource.Period.set(1e-4)
+        root.StreamDataSource.Period.set(1e-6)
         root.StreamDataSource.SourceEnable.set(True)
 
         # Wait until we have received 100k frames. This assumes the
