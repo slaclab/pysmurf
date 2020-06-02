aa813dba502503d28a7d69c0b1207d69fb4f0898
eyyoung24 <33497004+eyyoung24@users.noreply.github.com>
2020-06-01 20:17:42 -0700

    More optionality

--- python/pysmurf/client/debug/smurf_noise.py
+++ python/pysmurf/client/debug/smurf_noise.py
@@ -1804,7 +1804,8 @@ class SmurfNoiseMixin(SmurfBase):
     def take_noise_high_bandwidth(self, band, channel, freq=None,
         tone_power=None, nsamp=2**25, nperseg=2**18, make_plot=True,
         show_plot=False, save_plot=True, band_off=True,
-        reoptimize_resonator=True, save_psd=False):
+        run_serial_gradient_descent=True, run_serial_eta_scan=True,
+        save_psd=False):
         """
         This script is shamelessly stolen from Max. This tunes up a single
         resonator and takes data in single_channel_readout mode, with is 2.4 MHz.
@@ -1831,9 +1832,10 @@ class SmurfNoiseMixin(SmurfBase):
         # Turn on single tone
         band, channel = self.set_fixed_tone(freq, tone_power)
 
-        if reoptimize_resonator:
-            self.log('Reoptimizing resonator')
+        # Run optimization
+        if run_serial_gradient_descent:
             self.run_serial_gradient_descent(band)
+        if run_serial_eta_scan:
             self.run_serial_eta_scan(band)
 
 
