8e2d65b4576e1eedc8c92c70902530ec1a84b4a3
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-07-23 14:58:35 -0700

    Remove trailing white spaces and convert tabs to spaces

--- scratch/stephen/full_band_response_AMCatten.py
+++ scratch/stephen/full_band_response_AMCatten.py
@@ -11,19 +11,19 @@ n_scan_per_band=5
 wait_btw_bands_sec=5
 
 #Set all attenuators to 0 just to be safe
-for x in range (4) 
-	S.set_att_uc(i,0)
-	S.set_att_dc(i,0)
+for x in range (4)
+    S.set_att_uc(i,0)
+    S.set_att_dc(i,0)
 
 #Low band boards have fewer attenuation settings
 BoardType = raw_input("Enter board type <high> or <low>:" ).strip()
 if BoardType == "high":
-	att=[0,1,2,4,8,16,31]
+    att=[0,1,2,4,8,16,31]
 elif BoardType =="low":
-	att=[0,1,2,4]
+    att=[0,1,2,4]
 else:
-	print "No board type selected, aborting test"
-        return False	
+    print "No board type selected, aborting test"
+    return False
 
 timestamp=S.get_timestamp()
 bands=S.config.get('init').get('bands')
@@ -31,42 +31,42 @@ fig, ax = plt.subplots(ncols=len(att), sharex=True) #one plot per attenuation se
 pan_loc=0 #column location
 
 for att in att;
-	for x in range(4)
-		S.set_att_uc(x, att)
-		S.set_att_dc(x, att)
-	time.sleep(.1) #wait a sec for attenuators to change
-	resp_dict={}
-	for band in bands:
-	    print(' ')
-	    print(' ')
-	    print(f'Band {band}')
-	    print(' ')
-	    print(' ')
-	    resp_dict[band]={}
-	    resp_dict[band]['fc']=S.get_band_center_mhz(band)
-	
-	    f,resp=S.full_band_resp(band=band, make_plot=False, show_plot=Fals e, n_scan=n_scan_per_band, timestamp=timestamp,correct_att=False)
-	    resp_dict[band]['f']=f
-	    resp_dict[band]['resp']=resp
-	    
-	    time.sleep(wait_btw_bands_sec)
-	
-	ax[0,pan_loc].set_title(f'Full band response for attenuation={x} , {timestamp}')
-	last_angle=None
-	for band in bands:
-	    f_plot=resp_dict[band]['f']/1e6
-	    resp_plot=resp_dict[band]['resp']
-	    plot_idx = np.where(np.logical_and(f_plot>-250, f_plot<250))
-	    ax[0,pan_loc].plot(f_plot[plot_idx]+resp_dict[band]['fc'], np.log10(np.abs(resp_plot[plot_idx])),label=f'b{band}')
-	   		    
-	ax[0,pan_loc].legend(loc='lower left',fontsize=8)
-	ax[0,pan_loc].set_ylabel("log10(abs(Response))")
-	ax[0,pan_loc].set_xlabel('Frequency [MHz]')
+    for x in range(4)
+        S.set_att_uc(x, att)
+        S.set_att_dc(x, att)
+    time.sleep(.1) #wait a sec for attenuators to change
+    resp_dict={}
+    for band in bands:
+        print(' ')
+        print(' ')
+        print(f'Band {band}')
+        print(' ')
+        print(' ')
+        resp_dict[band]={}
+        resp_dict[band]['fc']=S.get_band_center_mhz(band)
 
-	for x in range(4)
-		S.set_att_uc(x, 0)
-		S.set_att_dc(x, 0)
-    	pan_loc++
+        f,resp=S.full_band_resp(band=band, make_plot=False, show_plot=Fals e, n_scan=n_scan_per_band, timestamp=timestamp,correct_att=False)
+        resp_dict[band]['f']=f
+        resp_dict[band]['resp']=resp
+
+        time.sleep(wait_btw_bands_sec)
+
+    ax[0,pan_loc].set_title(f'Full band response for attenuation={x} , {timestamp}')
+    last_angle=None
+    for band in bands:
+        f_plot=resp_dict[band]['f']/1e6
+        resp_plot=resp_dict[band]['resp']
+        plot_idx = np.where(np.logical_and(f_plot>-250, f_plot<250))
+        ax[0,pan_loc].plot(f_plot[plot_idx]+resp_dict[band]['fc'], np.log10(np.abs(resp_plot[plot_idx])),label=f'b{band}')
+
+    ax[0,pan_loc].legend(loc='lower left',fontsize=8)
+    ax[0,pan_loc].set_ylabel("log10(abs(Response))")
+    ax[0,pan_loc].set_xlabel('Frequency [MHz]')
+
+    for x in range(4)
+        S.set_att_uc(x, 0)
+        S.set_att_dc(x, 0)
+        pan_loc++
 
 
 save_name = '{}_full_band_resp_atten.png'.format(timestamp)
@@ -75,7 +75,7 @@ plt.title(save_name)
 plt.tight_layout()
 
 print(f'Saving plot to {os.path.join(S.plot_dir, save_name)}')
-plt.savefig(os.path.join(S.plot_dir, save_name), 
+plt.savefig(os.path.join(S.plot_dir, save_name),
             bbox_inches='tight')
 plt.show()
 
