495092f2ccbd0f44fc9c90b72e2dafb3b8e4d8b6
Shawn W. Henderson <shawn@slac.stanford.edu>
2020-08-10 23:41:22 -0700

    Finished adding docstrings to atca_monitor subroutines.

--- python/pysmurf/client/command/smurf_atca_monitor.py
+++ python/pysmurf/client/command/smurf_atca_monitor.py
@@ -13,20 +13,34 @@
 # copied, modified, propagated, or distributed except according to the terms
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
+"""Defines the :class:`SmurfAtcaMonitorMixin` class."""
 from pysmurf.client.base import SmurfBase
 
 class SmurfAtcaMonitorMixin(SmurfBase):
+    """Mixin providing interface with the smurf_atca_monitor server.
 
-    _write_atca_monitor_state_reg = ":Crate:WriteState"
+    This Mixin provides the pysmurf interface to the atca_monitor
+    registers.  The smurf_atca_monitor server is a Rogue application
+    which uses IPMI to monitor information from the ATCA system
+    [#smurf_atca_monitor]_.  The smurf_atca_monitor server must be
+    running or all queries will timeout and return `None`.
+
+    References
+    ----------
+    .. [#smurf_atca_monitor] https://github.com/slaclab/smurf-atca-monitor
+
+    """
 
     def write_atca_monitor_state(self, val, **kwargs):
-        """
+        """Writes atca_monitor state to yml file.
+
         Writes all current ATCA monitor values to a yml file.
 
         Args
         ----
         val : str
            The path (including file name) to write the yml file to.
+
         """
         self._caput(
             self.shelf_manager + self._write_atca_monitor_state_reg,
@@ -36,7 +50,35 @@ class SmurfAtcaMonitorMixin(SmurfBase):
 
     def get_board_temp_fpga(
             self, slot_number=None, atca_epics_root=None, **kwargs):
-        """
+        r"""Returns the AMC carrier board temperature.
+
+        Args
+        ----
+        slot_number : int or None, optional, default None
+            The crate slot number that the AMC carrier is installed
+            into.  If None, defaults to the
+            :class:`~pysmurf.client.base.smurf_control.SmurfControl`
+            class attribute
+            :attr:`~pysmurf.client.base.smurf_control.SmurfControl.slot_number`.
+        atca_epics_root : str or None, optional, default None
+            ATCA monitor server application EPICS root.  If None,
+            defaults to the
+            :class:`~pysmurf.client.base.smurf_control.SmurfControl`
+            class attribute
+            :attr:`~pysmurf.client.base.smurf_control.SmurfControl.shelf_manager`.
+            For typical systems, atca_epics_root is the name of the
+            shelf manager which for default systems is
+            'shm-smrf-sp01'.
+        \**kwargs
+            Arbitrary keyword arguments.  Passed directly to the
+            `_caget` call.
+
+        Returns
+        -------
+        float or None
+            AMC carrier board temperature in Celsius.  If None, either
+            the EPICS query timed out, or the atca_monitor server
+            isn't running.
         """
         if slot_number is None:
             slot_number=self.slot_number
@@ -50,7 +92,35 @@ class SmurfAtcaMonitorMixin(SmurfBase):
 
     def get_board_temp_rtm(
             self, slot_number=None, atca_epics_root=None, **kwargs):
-        """
+        r"""Returns the RTM board temperature.
+
+        Args
+        ----
+        slot_number : int or None, optional, default None
+            The crate slot number that the RTM is installed into.  If
+            None, defaults to the
+            :class:`~pysmurf.client.base.smurf_control.SmurfControl`
+            class attribute
+            :attr:`~pysmurf.client.base.smurf_control.SmurfControl.slot_number`.
+        atca_epics_root : str or None, optional, default None
+            ATCA monitor server application EPICS root.  If None,
+            defaults to the
+            :class:`~pysmurf.client.base.smurf_control.SmurfControl`
+            class attribute
+            :attr:`~pysmurf.client.base.smurf_control.SmurfControl.shelf_manager`.
+            For typical systems, atca_epics_root is the name of the
+            shelf manager which for default systems is
+            'shm-smrf-sp01'.
+        \**kwargs
+            Arbitrary keyword arguments.  Passed directly to the
+            `_caget` call.
+
+        Returns
+        -------
+        float or None
+            RTM board temperature in Celsius.  If None, either the
+            EPICS query timed out, or the atca_monitor server isn't
+            running.
         """
         if slot_number is None:
             slot_number=self.slot_number
@@ -64,7 +134,41 @@ class SmurfAtcaMonitorMixin(SmurfBase):
 
     def get_junction_temp_fpga(
             self, slot_number=None, atca_epics_root=None, **kwargs):
-        """
+        r"""Returns FPGA junction temperature.
+
+        A proxy for the FPGA die temperature - probably from a sensor
+        on the FPGA.  If you are looking at this, you probably should
+        be looking at
+        :func:`~pysmurf.client.command.smurf_command.SmurfCommandMixin.get_fpga_temp`
+        instead, which we think is more reliable.
+
+        Args
+        ----
+        slot_number : int or None, optional, default None
+            The crate slot number that the FPGA carrier is installed
+            into.  If None, defaults to the
+            :class:`~pysmurf.client.base.smurf_control.SmurfControl`
+            class attribute
+            :attr:`~pysmurf.client.base.smurf_control.SmurfControl.slot_number`.
+        atca_epics_root : str or None, optional, default None
+            ATCA monitor server application EPICS root.  If None,
+            defaults to the
+            :class:`~pysmurf.client.base.smurf_control.SmurfControl`
+            class attribute
+            :attr:`~pysmurf.client.base.smurf_control.SmurfControl.shelf_manager`.
+            For typical systems, atca_epics_root is the name of the
+            shelf manager which for default systems is
+            'shm-smrf-sp01'.
+        \**kwargs
+            Arbitrary keyword arguments.  Passed directly to the
+            `_caget` call.
+
+        Returns
+        -------
+        float or None
+            FPGA junction temperature in Celsius.  If None, either the
+            EPICS query timed out, or the atca_monitor server isn't
+            running.
         """
         if slot_number is None:
             slot_number=self.slot_number
@@ -73,7 +177,7 @@ class SmurfAtcaMonitorMixin(SmurfBase):
         return self._caget(
             f'{shelf_manager}:Crate:Slot_{slot_number}:' +
             self._junction_temp_fpga_reg,**kwargs)
-    
+
     _board_temp_amc_reg = 'BoardTemp:AMC{}'
 
     def get_board_temp_amc(self, bay, slot_number=None,
@@ -105,9 +209,11 @@ class SmurfAtcaMonitorMixin(SmurfBase):
 
         Returns
         -------
-        float
-            AMC board temperature in Celsius.
-        """        
+        float or None
+            AMC board temperature in Celsius.  If None, either the
+            EPICS query timed out, or the atca_monitor server isn't
+            running.
+        """
         if slot_number is None:
             slot_number=self.slot_number
         if atca_epics_root is None:
@@ -150,8 +256,10 @@ class SmurfAtcaMonitorMixin(SmurfBase):
 
         Returns
         -------
-        str
+        str or None
             AMC asset tag for the requested bay *e.g.* 'C03-A01-01'.
+            If None, either the EPICS query timed out, or the
+            atca_monitor server isn't running.
         """
         if slot_number is None:
             slot_number=self.slot_number
