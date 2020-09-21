0ab9f3dbbe6d3ff5394c09bf310940a122055766
eyyoung24 <33497004+eyyoung24@users.noreply.github.com>
2020-09-21 15:18:22 -0700

    Adding lots of functions to get PVs

--- python/pysmurf/client/command/smurf_timing.py
+++ python/pysmurf/client/command/smurf_timing.py
@@ -34,8 +34,104 @@ class SmurfTimingMixin(SmurfBase):
     # To do - Move to base
     timing_root = "TPG:SMRF:1:"
 
-    _time_skew = "TIMESKEW"
-    def get_time_skew(self, **kwargs):
+    _ckst = "CKST"
+
+    def get_timing_state(self, **kwargs):
         """
+        """
+        return self._caget(self.timing_root + self._ckst, **kwargs)
+
+
+    _time_skew = "TIMESKEW"
+
+    def get_timing_time_skew(self, **kwargs):
+        """ Time skew between NPT and TPG (in micro-second)
+
+        Returns
+        -------
+        skew : float
+            Time between NPT and TPG in micro-seconds
         """
         return self._caget(self.timing_root + self._time_skew, **kwargs)
+
+    _ratetxclk = "RATETXCLK"
+
+    def get_timing_clock_rate(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._ratetxclk, **kwargs)
+
+    _countpll = "COUNTPLL"
+
+    def get_timing_pll_count(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._countpll, **kwargs)
+
+    _countbrt = "COUNTBRT"
+
+    def get_timing_base_rate(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._countbrt, **kwargs)
+
+    _countsyncerr = "COUNTSYNCERR"
+
+    def get_timing_sync_err(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._countsyncerr, **kwargs)
+
+    _countintv = "COUNTINTV"
+
+    def get_timing_interval_counter(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._countintv, **kwargs)
+
+    _phyreadyrx = "PHYREADYRX"
+
+    def get_timing_phyreadyrx(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._phyreadyrx, **kwargs)
+
+    _phyreadytx = "PHYREADYTX"
+
+    def get_timing_phyreadytx(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._phyreadytx, **kwargs)
+
+    _loclnkready = "LOCLNKREADY"
+
+    def get_timing_loclnkready(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._loclnkready, **kwargs)
+
+    _remlnkready = "REMLNKREADY"
+
+    def get_timing_remlnkready(self, **kwargs):
+        """
+        """
+        return self._caget(self.timing_root + self._remlnkready, **kwargs)
+
+    def get_timing_all(self):
+        """
+        Polls all the timing PVs
+        """
+        timing_status = {}
+        timing_status['timing_state'] = self.get_timing_state()
+        timing_status['time_skew'] = self.get_timing_time_skew()
+        timing_status['clock_rate'] = self.get_timing_clock_rate()
+        timing_status['pll_count'] = self.get_timing_pll_count()
+        timing_status['base_rate'] = self.get_timing_base_rate()
+        timing_status['sync_err'] = self.get_timing_sync_err()
+        timing_status['interval_counter'] = self.get_timing_interval_counter()
+        timing_status['phyreadyrx'] = self.get_timing_phyreadyrx()
+        timing_status['phyreadytx'] = self.get_timing_phyreadytx()
+        timing_status['loclnkready'] = self.get_timing_loclnkready()
+        timing_status['remlnkready'] = self.get_timing_remlnkready()
+
+        return timing_status
