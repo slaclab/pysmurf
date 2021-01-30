ae993c21a786f258dee6d8fd474110b2e38029f7
Edward Young <33497004+eyyoung24@users.noreply.github.com>
2021-01-29 17:30:23 -0800

    Fixing more smurf_cmd.py

--- python/pysmurf/client/command/smurf_cmd.py
+++ python/pysmurf/client/command/smurf_cmd.py
@@ -18,30 +18,27 @@ import time
 
 import numpy as np
 
-import pysmurf
+import pysmurf.client
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-cfg_filename = 'experiment_k2umux.cfg'
 
+cfg_filename = os.path.join('/usr/local/src/pysmurf/', 'cfg_files', 'stanford',
+                            'experiment_fp31_cc03-02_lbOnlyBay0.cfg')
 
 """
 A function that mimics mce_cmd. This allows the user to run specific pysmurf
 commands from the command line.
 """
 def make_runfile(output_dir, row_len=60, num_rows=60, data_rate=60,
-                 num_rows_reported=60):
+    num_rows_reported=60):
     """
     Make the runfile
     """
-    #S.log('Making pysmurf object')
-    S = pysmurf.client.SmurfControl(cfg_file=os.path.join(os.path.dirname(__file__),
-        '..', 'cfg_files' , cfg_filename), smurf_cmd_mode=True, setup=False)
+    S = pysmurf.client.SmurfControl(cfg_file=cfg_filename, smurf_cmd_mode=True, setup=False)
 
     S.log('Making Runfile')
 
-    # 20181119 dB, modified to use the correct format runfile.
-    #with open(os.path.join(os.path.dirname(__file__),"runfile/runfile_template.txt")) as f:
     with open(os.path.join(os.path.dirname(__file__),
             "runfile/runfile.default.bicep53")) as f:
         lines = f.readlines()
@@ -76,10 +73,8 @@ def make_runfile(output_dir, row_len=60, num_rows=60, data_rate=60,
             line_holder.append(line)
 
     full_path = os.path.join(output_dir,
-                             f'smurf_status_{S.get_timestamp()}.txt')
+        f'smurf_status_{S.get_timestamp()}.txt')
 
-    #20181119 mod by dB to dump content of runfile, not path of runfile
-    #print(full_path)
     for line in line_holder:
         print(line)
     with open(full_path, "w") as f1:
@@ -88,43 +83,48 @@ def make_runfile(output_dir, row_len=60, num_rows=60, data_rate=60,
     S.log(f"Writing to {full_path}")
     sys.stdout.writelines(line_holder)
 
-def start_acq(S, num_rows, num_rows_reported, data_rate, row_len):
+def start_acq(S):
     """
-    """
-    S.log('Setting PVs for streaming header')
-    S.set_num_rows(num_rows, write_log=True)
-    S.set_num_rows_reported(num_rows_reported, write_log=True)
-    S.set_data_rate(data_rate, write_log=True)
-    S.set_row_len(row_len, write_log=True)
+    Start an acquisition
 
+    Args
+    ----
+    S : SmurfControl
+        The SmurfControl object used to issue commands
+    """
+    # Need to build out streaming header
     S.log('Starting streaming data')
-    S.set_smurf_to_gcp_stream(True, write_log=True)
     S.set_stream_enable(1, write_log=True)
 
 def stop_acq(S):
     """
+    Stop the acquisition
+
+    Args
+    ----
+    S : SmurfControl
+        The SmurfControl object used to issue commands
     """
     np.array(S.config.get('init').get('bands'))
     S.log('Stopping streaming data')
-    #for b in bands:
-    #    S.set_stream_enable(0)
-    S.set_smurf_to_gcp_stream(False, write_log=True)
+    S.set_stream_enable(False)
 
-def acq_n_frames(S, num_rows, num_rows_reported, data_rate,
-        row_len, n_frames):
+def acq_n_frames(S, n_frames):
     """
     Sends the amount of data requested by the user in units of n_frames.
 
     Args
     ----
+    S : SmurfControl
+        The SmurfControl object used to issue commands
     n_frames : int
         The number of frames to keep data streaming on.
     """
-    start_acq(S, num_rows, num_rows_reported, data_rate, row_len)
+    start_acq(S)
     make_runfile(S.output_dir, num_rows=num_rows, data_rate=data_rate,
         row_len=row_len, num_rows_reported=num_rows_reported)
-    sample_rate = 50E6/num_rows/data_rate/row_len
-    wait_time = n_frames/sample_rate
+    sample_rate = 50E6 / num_rows / data_rate / row_len
+    wait_time = n_frames / sample_rate
     time.sleep(wait_time)
     stop_acq(S)
 
@@ -133,6 +133,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--epics-prefix', help='The epics root',
+                        action='store', default=None, type=str)
+    
     # Offline mode
     parser.add_argument('--offline', help='For offline debugging',
         default=False, action='store_true')
@@ -167,8 +170,8 @@ if __name__ == "__main__":
         type=float, help="Interval between up and down steps.")
 
     # IV commands
-    parser.add_argument('--slow-iv', action='store_true', default=False,
-        help='Take IV curve using the slow method.')
+    parser.add_argument('--run-iv', action='store_true', default=False,
+        help='Take IV curve.')
     parser.add_argument('--plc', action='store_true', default=False,
         help='Take partial load curve.')
     parser.add_argument('--iv-band', action='store', type=int, default=-1,
@@ -244,7 +247,7 @@ if __name__ == "__main__":
     offline = args.offline
 
     # Check for too many commands
-    n_cmds = (args.log is not None) + args.tes_bias + args.slow_iv + \
+    n_cmds = (args.log is not None) + args.tes_bias + args.run_iv + \
         args.plc + args.tune + args.start_acq + args.stop_acq + \
         args.last_tune + (args.use_tune is not None) + args.overbias_tes + \
         args.bias_bump + args.soft_reset + args.make_runfile + args.setup + \
@@ -252,9 +255,11 @@ if __name__ == "__main__":
     if n_cmds > 1:
         sys.exit(0)
 
-    S = pysmurf.client.SmurfControl(cfg_file=os.path.join(os.path.dirname(__file__),
-        '..', 'cfg_files' , cfg_filename), smurf_cmd_mode=True, setup=False,
-        offline=offline)
+    epics_prefix = args.epics_prefix
+        
+    S = pysmurf.client.SmurfControl(epics_root=epics_prefix,
+                                    cfg_file=cfg_filename, smurf_cmd_mode=True,
+                                    setup=False, offline=offline)
 
     if args.log is not None:
         S.log(args.log)
@@ -286,7 +291,7 @@ if __name__ == "__main__":
                 bias_voltage_array = [float(bias) for bias in bias_voltage_str.split(" ")]
                 bias_voltage_array = np.array(bias_voltage_array)
             else:
-                bias_voltage_array = np.zeros((8,)) # hard-coded number of bias groups
+                bias_voltage_array = np.zeros((S._n_biasgroups,))
                 bias_voltage_array[S.all_groups] = bias_voltage # all_groups from cfg
             S.set_tes_bias_bipolar_array(bias_voltage_array, write_log=True)
         else:
@@ -296,18 +301,20 @@ if __name__ == "__main__":
     if args.overbias_tes:
 
         if S.high_current_mode_bool: # best thing I could think of, sorry -CY
-            tes_bias=2. # drop down to 2V to wait
+            tes_bias = 2. # drop down to 2V to wait
         else:
-            tes_bias=19.9 # factor of 10ish from above
+            tes_bias = 19.9 # factor of 10ish from above
 
         if args.bias_group < 0:
-            S.overbias_tes_all(overbias_wait=args.overbias_tes_wait, bias_groups=S.all_groups,
+            S.overbias_tes_all(overbias_wait=args.overbias_tes_wait,
+                bias_groups=S.all_groups,
                 high_current_mode=S.high_current_mode_bool, tes_bias=tes_bias)
         else:
-            S.overbias_tes(args.bias_group, overbias_wait=args.overbias_tes_wait,
+            S.overbias_tes(args.bias_group,
+                overbias_wait=args.overbias_tes_wait,
                 high_current_mode=S.high_current_mode_bool, tes_bias=tes_bias)
 
-    if args.slow_iv:
+    if args.run_iv:
         iv_bias_high = args.iv_bias_high
         iv_bias_low = args.iv_bias_low
         iv_bias_step = args.iv_bias_step
@@ -325,14 +332,14 @@ if __name__ == "__main__":
             iv_bias_step = np.abs(iv_bias_step)
 
         if args.bias_group < 0: # all
-            S.log('running slow IV on all bias groups')
+            S.log('running IV on all bias groups')
             S.run_iv(bias_groups=S.all_groups, wait_time=args.iv_wait_time,
                 bias_high=iv_bias_high, bias_low = iv_bias_low,
                 high_current_wait=args.iv_high_current_wait,
                 high_current_mode=S.high_current_mode_bool,
                 bias_step=iv_bias_step, make_plot=False)
         else: # individual bias group
-            S.log(f'running slow IV on bias group {args.bias_group}')
+            S.log(f'running IV on bias group {args.bias_group}')
             S.run_iv(bias_groups=np.array([args.bias_group]),
                 wait_time=args.iv_wait_time, bias_high=iv_bias_high,
                 bias_low=iv_bias_low,
@@ -383,11 +390,13 @@ if __name__ == "__main__":
                 args.data_rate, args.row_len, args.n_frames)
         else:
             S.log('Starting continuous acquisition')
-            start_acq(S, args.num_rows, args.num_rows_reported,
-                args.data_rate, args.row_len)
-            make_runfile(S.output_dir, num_rows=args.num_rows,
-                data_rate=args.data_rate, row_len=args.row_len,
-                num_rows_reported=args.num_rows_reported)
+            start_acq(S)
+            # Don't make runfiles for now. Need to figure out
+
+            
+            #make_runfile(S.output_dir, num_rows=args.num_rows,
+            #    data_rate=args.data_rate, row_len=args.row_len,
+            #    num_rows_reported=args.num_rows_reported)
             # why are we making a runfile though? do we intend to dump it?
 
     if args.stop_acq:
@@ -396,7 +405,7 @@ if __name__ == "__main__":
     if args.soft_reset:
         S.log('Soft resetting')
         S.set_smurf_to_gcp_clear(True)
-        time.sleep(.1) # make this longer, maybe? just a thought. it lasts ~15s in MCE
+        time.sleep(.1)
         S.set_smurf_to_gcp_clear(False)
 
     if args.setup:
