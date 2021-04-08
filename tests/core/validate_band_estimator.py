7a6f883de3170ea763f3a637379a8b675d81379f
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-04-07 20:07:13 -0700

    validate_band_estimator.py: Add the test of the BandPhaseFeedback device, plus some other improvements

--- tests/core/validate_band_estimator.py
+++ tests/core/validate_band_estimator.py
@@ -1,10 +1,57 @@
 #!/usr/bin/env python3
 
+import argparse
 import math
 import numpy as np
 import scipy.signal as signal
-from matplotlib import pyplot as plt
 
+# Input arguments
+parser = argparse.ArgumentParser(description='Test the SmurfProcessor Filter.')
+# Filter order
+parser.add_argument('--filter-order',
+        type=int,
+        default=4,
+        dest='filter_order',
+        help='Filter order')
+
+# Filter frequency
+parser.add_argument('--filter-freq',
+        type=float,
+        default=2*63/4000,
+        dest='filter_freq',
+        help='Filter order')
+
+# Number of generated points
+parser.add_argument('--num-samples',
+        type=int,
+        default=10000,
+        dest='num_samples',
+        help='Number of point to generate')
+
+# Output directory
+parser.add_argument('--out-dir',
+        type=str,
+        default='/tmp',
+        dest='out_dir',
+        help='Directory to write the output data')
+
+# Generate plots
+parser.add_argument('--plot-data',
+        action='store_true',
+        dest='plot_data',
+        help='Plot the data')
+
+# Do not test the rogue device
+parser.add_argument('--no-rogue',
+        action='store_true',
+        dest='no_rogue',
+        help='Do not test the rogue device.')
+
+# Use a low pass filter before the estimator
+parser.add_argument('--with-filter',
+        action='store_true',
+        dest='with_filter',
+        help='Low pass filter the phase data before the estimator.')
 
 class BandToneModel():
     """
@@ -15,9 +62,8 @@ class BandToneModel():
 
     Args
     ----
-
     freq : float
-        List of tone frequencies.
+        List of tone frequencies, in GHz.
     jump_index : int, optional, default 10000
         Time index where the phase jump happens.
     """
@@ -28,7 +74,8 @@ class BandToneModel():
         if not isinstance(freq, list):
             freq = [freq]
 
-        self._freq = freq
+        # Convert to Hz
+        self._freq = [f * 1e9 for f in freq]
         self._num_tones = len(freq)
 
         # Create a list of tone models, one for each frequency
@@ -112,11 +159,11 @@ class BandToneModel():
             self._prev_tau = 0
 
             # Theta parameters. The _theta_jump will be added to theta at the jump phase
-            self._theta = -0.02
-            self._theta_jump = 0.03
+            self._theta = -2e-3
+            self._theta_jump = 3e-3
 
             # Noise amplitude (constant across the phase jump)
-            self.noise_ampl = 2e-3
+            self.noise_ampl = 2e-6
 
             # Sample counter
             self._sample_index = 0
@@ -194,14 +241,13 @@ class LowPassFilterBank():
 
     """
 
-    def __init__(self, num_ch):
-
+    def __init__(self, num_ch, **kwargs):
         self._num_ch = num_ch
 
         # Create a list of filter, one for each channel.
         self._filters = []
         for n in range(num_ch):
-            self._filters.append(self._LowPassFilter())
+            self._filters.append(self._LowPassFilter(**kwargs))
 
     def push(self, x):
         """
@@ -281,14 +327,16 @@ class BandParameterEstimator():
     ----
 
     freq : list
-        List of tone frequencies.
+        List of tone frequencies, in GHz.
     """
     def __init__(self, freq):
 
         # Make sure freq is a list
         if not isinstance(freq, list):
             freq = [freq]
-        self._freq = freq
+
+        # Convert to Hz
+        self._freq = [f * 1e9 for f in freq]
 
         # Make sure we have at least two tones
         self._num_tones = len(self._freq)
@@ -352,15 +400,25 @@ class BandParameterEstimator():
 
 
 if __name__ == "__main__":
-    # Total number of samples.
-    num_samples = 20000
+
+    # Parse input arguments
+    args = parser.parse_args()
+    num_samples = args.num_samples
+    filter_freq = args.filter_freq
+    filter_order = args.filter_order
+    plot_data = args.plot_data
+    no_rogue = args.no_rogue
+    with_filter = args.with_filter
+    input_data_file = f'{args.out_dir}/input.dat'
+    python_estimated_file = f'{args.out_dir}/out_python.dat'
+    smurf_estimated_file = f'{args.out_dir}/out_smurf.dat'
 
     # Phase jump index
     # We set it at the middle of the test
     jump_index = num_samples/2
 
-    # Tone frequencies
-    freq = [1e9, 2e9]
+    # Tone frequencies, in GHz
+    freq = [4, 4.1]
 
     # Arrays to hold the data
     phase = np.ndarray(shape=(num_samples, len(freq)))
@@ -372,46 +430,265 @@ if __name__ == "__main__":
     # Band model. We set the phase jump at the middle of the data set.
     band = BandToneModel(freq=freq, jump_index=jump_index)
 
-    # Low pass filter bank
-    lpf = LowPassFilterBank(num_ch=len(freq))
+    # Low pass filter bank, if enabled
+    if with_filter:
+        lpf = LowPassFilterBank(
+            num_ch=len(freq),
+            filter_order=filter_order,
+            filter_freq=filter_freq)
 
     # Software Feedback
     estimator = BandParameterEstimator(freq=freq)
 
+    # Generate the emulated data
+    print('Generating data... ', end='')
     for i in range(num_samples):
         # Generate a phase sample, and saved the tau and theta parameters from the model
         phase[i], tau_model[i], theta_model[i] = band.get_sample()
 
-        # Low-pass filter the data
-        phase_filtered[i] = lpf.push(phase[i])
+        if with_filter:
+            # Low-pass filter the data
+            phase_filtered[i] = lpf.push(phase[i])
+        else:
+            # Do not filter the data, just copy the same generated data
+            phase_filtered[i] = phase[i]
 
         # Run the software feedback
         estimation[i] = estimator.push(phase_filtered[i])
 
-    # Calculate the RMSEs between the model and estimations
-    rmse_tau = np.sqrt(np.square(np.subtract(tau_model[:, 0],estimation[:, 0])).mean())
-    rmse_theta = np.sqrt(np.square(np.subtract(theta_model[:, 0],estimation[:, 1])).mean())
+    print("Done")
+
+    # Write the estimator input data into a file.
+    # We convert it to "int32" as the SMuRF device will use it.
+    print(f'Writing data to "{input_data_file}"... ', end='')
+    np.savetxt(input_data_file, phase_filtered*(2**31), fmt='%i')
+    print('Done')
+
+    # Write the estimated data into a file.
+    print(f'Writing data to "{python_estimated_file}"... ', end='')
+    np.savetxt(python_estimated_file, estimation, fmt='%1.4e')
+    print('Done')
+
+    # Calculate the RMSE of python estimator, respect to the model data
+    print('RMSE of the python estimator:')
+    rmse_tau = np.sqrt(np.square(np.subtract(tau_model[:, 0], estimation[:, 0])).mean())
+    rmse_theta = np.sqrt(np.square(np.subtract(theta_model[:, 0], estimation[:, 1])).mean())
     print(f"Tau RMSE = {rmse_tau}")
     print(f"Theta RMSE = {rmse_theta}")
 
-    # Plot the data
-    plt.figure("Tone phase signal")
-    for i in range(len(freq)):
-        plt.plot(phase[:, i], label=f'Tone {i} phase')
-        plt.plot(phase_filtered[:, i], label=f'Tone {i} phase (low-pass filtered)')
-    plt.grid()
-    plt.legend()
+    # Test the SMuRF device, if enabled
+    if not no_rogue:
+        import sys
 
-    plt.figure("Phase slope (tau)")
-    plt.plot(tau_model[:, 0], label="From model")
-    plt.plot(estimation[:, 0], label="Estimated")
-    plt.grid()
-    plt.legend()
+        import rogue
+        import rogue.interfaces.stream
 
-    plt.figure("Phase offset (theta)")
-    plt.plot(theta_model[:, 0], label="From model")
-    plt.plot(estimation[:, 1], label="Estimated")
-    plt.grid()
-    plt.legend()
+        import pyrogue
+        import pysmurf
+        import pysmurf.core.devices
+        import pysmurf.core.transmitters
 
-    plt.show()
+        class LocalDataSource(pyrogue.Device):
+            """
+            Class to stream data.
+
+            Args
+            ----
+            name : str
+                Device name.
+            description : str
+                Device description.
+            dataSize : int, optional, default 16
+                Data size in bits to use in the frame
+            """
+            def __init__(self,
+                 name="LocalDataSource",
+                 description="Local data source",
+                 dataSize=16,
+                 **kwargs):
+                pyrogue.Device.__init__(self, name=name, description=description, **kwargs)
+                self._data_master = self._DataMaster(dataSize=dataSize)
+
+                self.add(pyrogue.LocalVariable(
+                    name='FrameCnt',
+                    description='Number of sent frames',
+                    mode='RO',
+                    value=0,
+                    localGet = self._data_master.get_frame_cnt))
+
+                self.add(pyrogue.LocalCommand(
+                    name='SendData',
+                    description='Send data pass as an argument',
+                    value='',
+                    function=lambda arg: self._data_master.sendData(data=arg)))
+
+            def _getStreamMaster(self):
+                """
+                Method called by streamConnect, streamTap and streamConnectBiDir to access master.
+                """
+                return self._data_master
+
+            class _DataMaster(rogue.interfaces.stream.Master):
+                """
+                A Rogue master device, used to stream the data.
+
+                Args
+                ----
+                dataSize : int
+                    Data size in bits to use in the frame
+                """
+                def __init__(self, dataSize):
+                    super().__init__()
+                    self._frame_cnt = 0
+                    self._data_size = dataSize
+
+                def get_frame_cnt(self):
+                    """
+                    Get the number of sent frames
+                    """
+                    return self._frame_cnt
+
+                def sendData(self, data):
+                    """
+                    Send a Rogue Frame. The frame contains the SMuRF header and the
+                    input data points in contiguous channels. The SMuRF header will
+                    be only partially filled, containing only the number of channels,
+                    and a the frame counter words.
+
+                    Args
+                    ----
+                    data : list
+                        Input data (must be of size 'dataSize').
+                    """
+
+                    # Data size in bytes
+                    data_size_bytes = int(self._data_size / 8)
+
+                    # Request a frame to hold an SMuRF frame
+                    frame = self._reqFrame(128+data_size_bytes*4096, True)
+
+                    # Fill the frame with zeros
+                    frame.write( bytearray([0]*(128+data_size_bytes*4096)), 0 )
+
+                    # Write the number of channels
+                    frame.write( bytearray((4096).to_bytes(4, sys.byteorder)), 4)
+
+                    # Write the frame counter into the header
+                    frame.write( bytearray(self._frame_cnt.to_bytes(4, sys.byteorder)), 84)
+
+                    # Write the data into the first channel
+                    index = 128 # This is the start of the data area in a SMuRF frame
+                    for d in list(map(int, data.split())):
+                        frame.write( bytearray(d.to_bytes(data_size_bytes, sys.byteorder, signed=True)), index)
+                        index += data_size_bytes
+
+                    # Send the frame
+                    self._sendFrame(frame)
+
+                    # Update the frame counter
+                    self._frame_cnt =  self._frame_cnt + 1
+
+        class LocalRoot(pyrogue.Root):
+            """
+            Local root device.
+
+            It contains the BandPhaseFeedback device, connected to our LocalDataSource
+            data source. It will read the data from the input data file generated previously,
+            and send its contents through the BandPhaseEstimator device, while reading the
+            estimation result variables and save then into an output file, one data set at
+            a time.
+            """
+            def __init__(self, **kwargs):
+                pyrogue.Root.__init__(self, name="AMCc", initRead=True, pollEn=True, **kwargs)
+
+                # Use the LocalDataSource device as a stream data source. We will use int32 data type.
+                self._streaming_stream = LocalDataSource(dataSize=32)
+                self.add(self._streaming_stream)
+
+                # Add a BandPhaseFeedback device
+                self._band_phase_feedback = pysmurf.core.feedbacks.BandPhaseFeedback(name="BandPhaseFeedback", band=0)
+                self.add(self._band_phase_feedback)
+
+                # Connect the LocalDataSource data source to the BandPhaseFeedback
+                pyrogue.streamConnect(self._streaming_stream, self._band_phase_feedback)
+
+        # Send the input data through the BandPhaseFeedback device.
+        # We send one frame at a time, while reading the estimator results from its
+        # variables.
+        print('Starting the Rogue root, and send the same data through it')
+        with LocalRoot() as root:
+            print('  Enabling the band phase feedback device... ', end='')
+            root.BandPhaseFeedback.Disable.set(False)
+            print('Done')
+
+            print('  Setting the tone channels... ', end='')
+            root.BandPhaseFeedback.toneChannels.set([0, 1])
+            print('Done')
+
+            print('  Setting the tone frequencies... ', end='')
+            root.BandPhaseFeedback.toneFrequencies.set(freq)
+            print('Done')
+
+            print('  Sending the data, and writing results... ', end='')
+            with open(input_data_file, 'r') as f_in, open(smurf_estimated_file, 'w') as f_out:
+                for data in f_in:
+                    # Send a frame with a data set from the input file
+                    root.LocalDataSource.SendData.call(arg=data)
+
+                    # Read the estimator results
+                    tau = root.BandPhaseFeedback.Tau.get()
+                    theta = root.BandPhaseFeedback.Theta.get()
+
+                    # Write the results to the output file
+                    f_out.write(f'{tau} {theta}\n')
+
+            print('Done')
+
+            # Print the number of frames sent though the estimator device
+            print(f'  Number of frame sent = {root.BandPhaseFeedback.FrameCnt.get()}')
+
+        # Load the results obtained using the BandPhaseFeedback device
+        print('Reading results... ', end='')
+        smurf_estimation = np.loadtxt(smurf_estimated_file, dtype='float')
+        print('Done.')
+
+        # We converted the data to 'int32' before use it on the BandPhaseFeedback device,
+        # so we need to remove the 2^31 factor we introduce in the estimation results in
+        # order to be able to compare it directly with the data generated with the python
+        # estimator.
+        smurf_estimation = smurf_estimation * (2**-31)
+
+        # Calculate the RMSE between the python and SMuRF estimator
+        rmse_tau = np.sqrt(np.square(np.subtract(estimation[:,0], smurf_estimation[:,0])).mean())
+        rmse_theta = np.sqrt(np.square(np.subtract(estimation[:,1], smurf_estimation[:,1])).mean())
+        print('RMSE between python and SMuRF estimators:')
+        print(f'Tau = {rmse_tau}')
+        print(f'Theta = {rmse_theta}')
+
+    # Plot the data, if enable
+    if plot_data:
+        from matplotlib import pyplot as plt
+
+        # Plot the tone signals
+        plt.figure("Tone phase signal")
+        for i in range(len(freq)):
+            plt.plot(phase[:, i], label=f'Tone {i} phase')
+            plt.plot(phase_filtered[:, i], label=f'Tone {i} phase (low-pass filtered)')
+        plt.grid()
+        plt.legend()
+
+        # Plot the generated and estimated 'tau' parameters
+        plt.figure("Phase slope (tau)")
+        plt.plot(tau_model[:, 0], label="From model")
+        plt.plot(estimation[:, 0], label="Estimated")
+        plt.grid()
+        plt.legend()
+
+        # Plot the generated and estimated 'theta' parameters
+        plt.figure("Phase offset (theta)")
+        plt.plot(theta_model[:, 0], label="From model")
+        plt.plot(estimation[:, 1], label="Estimated")
+        plt.grid()
+        plt.legend()
+
+        plt.show()
