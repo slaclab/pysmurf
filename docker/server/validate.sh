ce11f8ac1a144e1796dd913fe626aff0199070df
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-05-04 13:35:21 -0700

    Add scripts to generate the stable docker image from this repository

---
+++ docker/server/validate.sh
@@ -0,0 +1,241 @@
+#!/usr/bin/env bash
+
+# Validate definitions.sh
+
+#############
+# FUNCTIONS #
+#############
+# Check if a tag exists on a github public repository
+# Arguments:
+# - first: github public repository url,
+# - second: tag name
+check_if_public_tag_exist()
+{
+    local repo=$1
+    local tag=$2
+    git ls-remote --refs --tag ${repo} | grep -q refs/tags/${tag} > /dev/null
+}
+
+# Check if a asset file exist on a tag version on a github public repository
+# Arguments:
+# - first: github public repository url,
+# - second: tag name,
+# - third: asset file name
+check_if_public_asset_exist()
+{
+    local repo=$1
+    local tag=$2
+    local file=$3
+    curl --head --silent --fail ${repo}/releases/download/${tag}/${file} > /dev/null
+}
+
+# Check if a tag exists on a github pivate repository.
+# It requires ssh key authentication.
+# Arguments:
+# - first: github private repository url,
+# - second: tag name
+check_if_private_tag_exist()
+{
+    # Need to insert the token in the url
+    local repo=$(echo $1 | sed -e "s|https://github.com/|git@github.com:|g")
+    local tag=$2
+    git ls-remote --refs --tag ${repo} | grep -q refs/tags/${tag} > /dev/null
+}
+
+# Check if a asset file exist on a tag version on a github private repository.
+# It requires the access token to be defined in $GITHUB_TOKEN.
+# Arguments:
+# - first: github private repository url,
+# - second: tag name,
+# - third: asset file name
+check_if_private_asset_exist()
+{
+    local repo=$(echo $1 | sed -e "s|https://github.com|https://api.github.com/repos|g")
+    local tag=$2
+    local file=$3
+
+    # Search the asset ID in the specified release
+    local r=$(curl --silent --header "Authorization: token ${GITHUB_TOKEN}" "${repo}/releases/tags/${tag}")
+    eval $(echo "${r}" | grep -C3 "name.:.\+${file}" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+
+    # return is the asset tag was found
+    [ "${id}" ]
+}
+
+# Download the asset file on a tagged version on a github private repository.
+# It requires the access token to be defined in $GITHUB_TOKEN.
+# Arguments:
+# - first: github private repository url,
+# - second: tag name,
+# - third: asset file name
+get_private_asset()
+{
+    local repo=$(echo $1 | sed -e "s|https://github.com|https://api.github.com/repos|g")
+    local tag=$2
+    local file=$3
+
+    # Check if the asset exist, and get it's ID
+    check_if_private_asset_exist ${repo} ${tag} ${file} || exit 1
+
+    echo "Downloading ${file}..."
+
+    # Try to download the asset
+    curl --fail --location --remote-header-name --remote-name --progress-bar \
+         --header "Authorization: token ${GITHUB_TOKEN}" \
+         --header "Accept: application/octet-stream" \
+         "${repo}/releases/assets/${id}"
+}
+
+# Check if file exist on a tag version on a github repository
+# Arguments:
+# - first: github repository url,
+# - second: tag name,
+# - third: file name (must be a full path on that repository)
+check_if_file_exist()
+{
+    local repo=$1
+    local tag=$2
+    local file=$3
+    curl --head --silent --fail ${repo}/blob/${tag}/${file} > /dev/null
+}
+
+# Exit with an error message, and with return code = 1
+exit_on_error()
+{
+    echo
+    echo "Validation failed! The 'definitions.sh' file is incorrect!"
+    echo
+    exit 1
+}
+
+# Exist with a success message and with return core = 0
+exit_on_success()
+{
+    echo
+    echo "Success! The 'definitions.sh' file is correct!"
+    echo
+}
+
+#############
+# MAIN BODY #
+#############
+
+# Load the user definitions
+. definitions.sh
+
+# Extra definitions, generated from the user definitions
+zip_file_name=rogue_${fw_repo_tag}.zip
+
+# Validate if repositories were defined
+echo "======================================================================================"
+echo "Repository names validation"
+echo "======================================================================================"
+
+printf "Checking if firmware repository was defined...      "
+if [ -z ${fw_repo} ]; then
+    echo "Failed!"
+    echo
+    echo "Firmware repository not define! Please check that the variable 'fw_repo' is defined in the 'definitions.sh' file."
+    exit_on_error
+fi
+echo "${fw_repo}"
+
+printf "Checking if configuration repository was defined... "
+if [ -z ${config_repo} ]; then
+    echo "Failed!"
+    echo
+    echo "Configuration repository not define! Please check that the variable 'config_repo' is defined in the 'definitions.sh' file."
+    exit_on_error
+fi
+echo "${config_repo}"
+
+# At this point all repository name definition are correct.
+echo "Done! All repository names were defined!"
+echo
+
+# Validate the firmware version
+echo "======================================================================================"
+echo "Firmware version validation"
+echo "======================================================================================"
+
+printf "Checking if the tag version was defined...          "
+if [ -z ${fw_repo_tag} ]; then
+    echo "Failed!"
+    echo
+    echo "Firmware tag version not defined! Please check that the variable 'fw_repo_tag' is defined in the 'definitions.sh' file."
+    exit_on_error
+fi
+echo "${fw_repo_tag}"
+
+printf "Checking if MCS file name was defined...            "
+if [ -z ${mcs_file_name} ]; then
+    echo "Failed!"
+    echo
+    echo "MCS file name not defined! Please check that the variable 'mcs_file_name' is defined in the 'definitions.sh' file."
+    exit_on_error
+fi
+echo "${mcs_file_name}"
+
+printf "Checking if release exist...                        "
+check_if_private_tag_exist ${fw_repo} ${fw_repo_tag}
+if [ $? != 0 ]; then
+    echo "Failed!"
+    echo
+    echo "Release '${fw_repo_tag}' does not exist in repository '${fw_repo}'!"
+    exit_on_error
+fi
+echo "Release exist!"
+
+printf "Checking if MCS file is in the list of assets...    "
+check_if_private_asset_exist ${fw_repo} ${fw_repo_tag} ${mcs_file_name}
+if [ $? != 0 ]; then
+    echo "Failed!"
+    echo
+    echo "File '${mcs_file_name}' does not exist in the assets list of release '${fw_repo_tag}'!"
+    exit_on_error
+fi
+echo "File exist!"
+
+printf "Checking if ZIP file is in the list of assets...    "
+check_if_private_asset_exist ${fw_repo} ${fw_repo_tag} ${zip_file_name}
+if [ $? != 0 ]; then
+    echo "Failed!"
+    echo
+    echo "File '${zip_file_name}' does not exist in the assets list of release '${fw_repo_tag}'!"
+    exit_on_error
+fi
+echo "File exist!"
+
+# At this points all the definition of the firmware version are correct.
+echo "Done! A correct firmware version was defined"
+echo
+
+
+# Validate the configuration version
+echo "======================================================================================"
+echo "Configuration version validation"
+echo "======================================================================================"
+
+printf "Checking if the tag version was defined...          "
+if [ -z ${config_repo_tag} ]; then
+    echo "Failed!"
+    echo
+    echo "Configuration tag version not defined! Please check that the variable 'config_repo_tag' is defined in the 'definitions.sh' file."
+fi
+echo "${config_repo_tag}"
+
+printf "Checking if release exist...                        "
+check_if_public_tag_exist ${config_repo} ${config_repo_tag}
+if [ $? != 0 ]; then
+    echo "Failed!"
+    echo
+    echo "Release '${config_repo_tag}' does not exist in repository '${config_repo}'!"
+    exit_on_error
+fi
+echo "Release exist!"
+
+# At this point all the definitions of the configuration version are correct.
+echo "Done! A correct configuration version was defined"
+
+# At this point all definition where correct
+exit_on_success
