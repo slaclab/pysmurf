0ddb00950f53300d54f4b3581d1b1d91980f6bbe
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-05-04 15:39:40 -0700

    Try with a ssh key

--- docker/server/validate.sh
+++ docker/server/validate.sh
@@ -37,10 +37,9 @@ check_if_public_asset_exist()
 check_if_private_tag_exist()
 {
     # Need to insert the token in the url
-    local repo=$(echo $1 | sed -e "s|https://github.com/|https://"${GITHUB_TOKEN}"@github.com/|g")
+    local repo=$(echo $1 | sed -e "s|https://github.com/|git@github.com:|g")
     local tag=$2
-    echo ${repo}
-    git ls-remote --refs --tag ${repo} | grep -q refs/tags/${tag}
+    git ls-remote --refs --tag ${repo} | grep -q refs/tags/${tag} > /dev/null
 }
 
 # Check if a asset file exist on a tag version on a github private repository.
