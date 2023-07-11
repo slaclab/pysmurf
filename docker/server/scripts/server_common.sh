a0e56723903bd22d348472e7f71078af57e269f8
Ryan Herbst <rherbst@slac.stanford.edu>
2023-07-11 13:48:01 -0700

    Update

--- docker/server/scripts/server_common.sh
+++ docker/server/scripts/server_common.sh
@@ -399,7 +399,7 @@ validateCommType()
         comm_type='eth'
     else
         # Check if the communication type is invalid
-        if [ ${comm_type} != 'eth' ] && [ ${comm_type} != 'pcie' ]; then
+        if [ ${comm_type} != 'eth' ] && [ ${comm_type} != 'pcie' ] && [ ${comm_type} != 'emu' ]; then
             echo "Invalid communication type!."
             usage
         fi
@@ -649,6 +649,9 @@ detect_amc_board()
             elif [ ${type_str} == "A02" ]; then
                 echo "This is a HB board."
                 band_bay[$i]="hb"
+            elif [ ${type_str} == "A03" ]; then
+                echo "This is a TKID board."
+                band_bay[$i]="tkid"
             else
                 echo "Board type not supported."
                 echo
