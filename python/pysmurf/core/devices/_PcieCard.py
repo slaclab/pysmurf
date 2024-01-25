b594ab8f145f953b87cd8f3451523528ff0d3dc7
Agustin Romero <89935000+aer7@users.noreply.github.com>
2024-01-25 12:25:27 -0800

    Fixed import for pcie card

--- python/pysmurf/core/devices/_PcieCard.py
+++ python/pysmurf/core/devices/_PcieCard.py
@@ -41,7 +41,7 @@ class PcieDev():
     """
     def __init__(self, dev, name, description):
         import rogue.hardware.axi
-        from SmurfPcie import SmurfKcu1500RssiOffload10GbE as fpga
+        import SmurfKcu1500RssiOffload as fpga
         self._root = pyrogue.Root(name=name,description=description, pollEn='False',initRead='True')
         self._memMap = rogue.hardware.axi.AxiMemMap(dev)
         self._root.add(fpga.Core(memBase=self._memMap))
