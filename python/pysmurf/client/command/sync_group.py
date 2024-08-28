eb5d1c178e1dc801787c133625bf46a10cec4abb
Tristan Pinsonneault-Marotte <tristpinsm@gmail.com>
2024-08-28 07:57:02 -0700

    fix(SyncGroup): Crate listener before wait is called.

--- python/pysmurf/client/command/sync_group.py
+++ python/pysmurf/client/command/sync_group.py
@@ -15,6 +15,7 @@
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
 from pyrogue import VariableWait
+from time import time
 
 """
 This class is written by Mitch to read PVs simultanesouly
@@ -23,20 +24,32 @@ class SyncGroup(object):
     def __init__(self, pvs, client, timeout=30.0):
         self.pvnames = pvs
         self.timeout = timeout
-        self.pvs = []
+        self.pvs = {}
+        self.updated = {}
+        #self.vals = {}
         for pvname in pvs:
             node = client.root.getNode(pvname)
             if node is None:
                 raise ValueError(f"Failed to get node {pvname}")
-            self.pvs.append(node)
+            self.pvs[pvname] = node
+            self.updated[pvname] = False
+            node.addListener(self._receive_update)
 
-    def get_values(self):
-        return {self.pvnames[i]: self.pvs[i].get() for i in range(len(self.pvs))}
+    def _receive_update(self, path, val):
+        if path in self.pvs:
+            self.updated[path] = True
+            #self.vals[path] = val.get()
+
+    def get_values(self, read=False):
+        #if read or (not all(self.updated.values())):
+        #    return {self.pvnames[i]: self.pvs[i].get() for i in range(len(self.pvs))}
+        #else:
+        #    return self.vals
+        return {k: self.pvs[k].get() for k in self.pvs}
 
     # blocking wait for all to complete
     def wait(self):
-        VariableWait(
-            self.pvs,
-            lambda varValues: all([v.updated for v in varValues]),
-            timeout=self.timeout
-        )
+        done = lambda: all(self.updated.values())
+        start = time()
+        while not done() and ((time() - start) < self.timeout): 
+            continue
