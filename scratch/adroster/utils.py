43cefab02a529d62ad80089a9396611492811b40
cryo <cryo@$(hostname)>
2026-04-20 15:41:16 -0700

    cleaning up repo

--- scratch/adroster/utils.py
+++ scratch/adroster/utils.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
-
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -25,9 +22,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 from scipy.signal import butter, welch, filtfilt, periodogram, savgol_filter
-
-
-# In[2]:
 
 
 def welch_IQ(iq, fs,  plot=True, welch_nperseg=2**18, title='', show_plot=True):
@@ -52,10 +46,6 @@ def welch_IQ(iq, fs,  plot=True, welch_nperseg=2**18, title='', show_plot=True):
         if show_plot: plt.show()
     return
 
-
-# In[3]:
-
-
 def freqSweep(S, band, start_freq=-250, stop_freq=250, subband=None, tone_power=None, n_avg=1, makeplot=False):
     """
     Inputs:
@@ -70,16 +60,22 @@ def freqSweep(S, band, start_freq=-250, stop_freq=250, subband=None, tone_power=
     Outputs:
     freqs (ndarray): real-valued 1D array of frequenies (RF, not IF)
     resp_avg (ndarray): complex-valued 1D array of response
+
+    Note on using either sideband of the mixer:
+    Certain - signs need to be flipped to + signs when using the RHS of the mixer. As written, this is for a LO tuned to 750 MHz above\
+    the the desired center frequency. See comments on which signs to flip.
     """
     #adapted from S.find_freq()
     band_center = S.get_band_center_mhz(band)
     if subband is None:
-        start_subband = S.freq_to_subband(band, band_center + start_freq)[0]
-        stop_subband = S.freq_to_subband(band, band_center + stop_freq)[0]
+        #flip these two minuses to pluses whn using RHS of mixer
+        start_subband = S.freq_to_subband(band, band_center - start_freq)[0]
+        stop_subband = S.freq_to_subband(band, band_center - stop_freq)[0]
         step = 1
         if stop_subband < start_subband:
             step = -1
         subband = np.arange(start_subband, stop_subband+1, step)
+        #print(subband)
     else:
         sb, sbc = S.get_subband_centers(band)
         start_freq = sbc[subband[0]]
@@ -100,13 +96,14 @@ def freqSweep(S, band, start_freq=-250, stop_freq=250, subband=None, tone_power=
     S.log(f'Sweeping across frequencies {start_freq + band_center}MHz to {stop_freq + band_center}MHz. Averaging {n_avg} traces.')
     BW_MHz = 500
     freqs_per_channel = 31
-    n_channels = int(np.ceil((S.get_number_processed_channels(band))*(stop_freq - start_freq)/BW_MHz))
+    n_channels = int(np.ceil((S.get_number_processed_channels(band))*(stop_freq - start_freq)/BW_MHz)+1)
     resp_shape = (n_avg, freqs_per_channel*n_channels)
-    resp_arr = np.zeros(resp_shape, dtype = np.complex_)
+    #resp_arr = np.zeros(resp_shape, dtype = np.complex_)
+    resp_arr = []
     for n in range(n_avg):
         f, resp = S.full_band_ampl_sweep(band, subband, tone_power, n_read=2) #n_read doesn't actually do anything; must be legacy code from previous SMuRF implementations
         
-        #below is adapted from S.find_all_peak()
+        # below is adapted from S.find_all_peak()
         # Stack all the frequency and response data into a
         # Frequency array is the same on every iteration, so only need to find & sort frequencies once
         if n == 0:
@@ -125,6 +122,7 @@ def freqSweep(S, band, start_freq=-250, stop_freq=250, subband=None, tone_power=
                 if np.max(f_stack) > 1.0E8:
                     self.log('Frequency is probably in Hz. Converting to MHz')
                     scale = 1.0E-6
+                #flip these two minus signs to + signs when using RHS of mixer
                 freq_rf = bandCenterMHz - freqs*scale
             else:
                 freq_rf = bandCenterMHz - freqs
@@ -133,12 +131,15 @@ def freqSweep(S, band, start_freq=-250, stop_freq=250, subband=None, tone_power=
     
         # response is also interleaved, so sort it
         r_stack = r_stack[s]
+        #r_stack = r_stack[::-1]
 
         #append the sorted & stacked response array to resp_arr
-        resp_arr[n,:] = r_stack
+        #resp_arr[n,:] = r_stack
+        resp_arr.append(r_stack)
 
     #Frequencies are just f_stack; Average the response
 
+    resp_arr = np.array(resp_arr)
     resp_avg = np.mean(resp_arr, axis=0)
     
     # Break apart the data to find phase, deriv of phase
@@ -156,18 +157,7 @@ def freqSweep(S, band, start_freq=-250, stop_freq=250, subband=None, tone_power=
     #calculating rolling median. Adapted from S.find_peaks()   
     # Calculate the rolling median. This uses pandas. This is analogous to smoothing on a VNA.
     import pandas as pd
-    med_amp = pd.Series(amp).rolling(window=50, center=True, min_periods=1).median()
-
-    # convert frequencies from IF to the RF
-    if band is not None:
-        bandCenterMHz = S.get_band_center_mhz(band)
-        scale = 1
-        if np.max(f_stack) > 1.0E8:
-            self.log('Frequency is probably in Hz. Converting to MHz')
-            scale = 1.0E-6
-        freq_rf = bandCenterMHz - freqs*scale
-    else:
-        freq_rf = bandCenterMHz - freqs
+    med_amp = pd.Series(amp).rolling(window=50, center=True, min_periods=1).median() 
     
     #plotting
     if makeplot:
@@ -197,41 +187,32 @@ def freqSweep(S, band, start_freq=-250, stop_freq=250, subband=None, tone_power=
     
     return freq_rf, resp_avg
 
-
-# In[4]:
-
-
 # find the frequency between parity bands
-def lorentzian(f, f0, Gc, Gr, g, Omega):
+def lorentzian(f, f0, Gc, Gr, g, Omega, slope):
     Delta = 2*np.pi*(f - f0)
-    S21 = 1 - (Gc/2/g)*(1-Delta*1j/g)/(1+(Delta/g)**2+(2*np.pi*Omega)**2/g/Gr)
+    S21 = 1 - (Gc/2/g)*(1-Delta*1j/g)/(1+(Delta/g)**2+(2*np.pi*Omega)**2/g/Gr) + slope*Delta
     return S21
     
-def invertedDoubleLorentzian(f, f0_1, f0_2, Gc_1, GC_2, Gr_1, Gr_2, g_1, g_2, Omega_1, Omega_2, baseline, scale):
+def invertedDoubleLorentzian(f, f0_1, f0_2, Gc_1, GC_2, Gr_1, Gr_2, g_1, g_2, Omega_1, Omega_2, baseline, scale, slope):
     '''
     Eq. E22 in the design paper
     '''
-    lorentzian1 = lorentzian(f, f0_1, Gc_1, Gr_1, g_1, Omega_1)
-    lorentzian2 = lorentzian(f, f0_2, Gc_2, Gr_2, g_2, Omega_2)
+    lorentzian1 = lorentzian(f, f0_1, Gc_1, Gr_1, g_1, Omega_1, slope)
+    lorentzian2 = lorentzian(f, f0_2, Gc_2, Gr_2, g_2, Omega_2, slope)
     S21 = baseline + scale*(lorentzian1 + lorentzian2)
     return np.abs(S21)
 
-def negInvertedDoubleLorentzian(f, f0_1, f0_2, Gc_1, GC_2, Gr_1, Gr_2, g_1, g_2, Omega_1, Omega_2, baseline, scale):
+def negInvertedDoubleLorentzian(f, f0_1, f0_2, Gc_1, GC_2, Gr_1, Gr_2, g_1, g_2, Omega_1, Omega_2, baseline, scale, slope):
     '''
     Eq. E22 in the design paper
     '''
-    lorentzian1 = lorentzian(f, f0_1, Gc_1, Gr_1, g_1, Omega_1)
-    lorentzian2 = lorentzian(f, f0_2, Gc_2, Gr_2, g_2, Omega_2)
-    S21 = baseline + scale*(lorentzian1 + lorentzian2)
-    return -np.abs(S21)
-
-
-# In[5]:
-
+    return -invertedDoubleLorentzian(f, f0_1, f0_2, Gc_1, GC_2, Gr_1, Gr_2, g_1, g_2, Omega_1, Omega_2, baseline, scale, slope)
 
 def takeDebugData(S, band, channel, nsamp, plot=True, welch_nperseg=2**18, show_plot=True, channel_mode=0):
-    timestamp = S.get_timestamp() 
-    filename = f'{timestamp}_single_channel_b{band}ch{channel:03}'
+    timestamp = S.get_timestamp()
+    filename = f'{timestamp}_multi_channel_b{band}'
+    if channel is not None:
+        filename = f'{timestamp}_single_channel_b{band}ch{channel:03}'
     i,q,sync = S.take_debug_data(band=band,channel=channel,rf_iq=True,nsamp=nsamp,filename=filename, single_channel_readout=channel_mode) 
     i = i / (1.2)
     q = q / (-1.2)
@@ -245,19 +226,8 @@ def takeDebugData(S, band, channel, nsamp, plot=True, welch_nperseg=2**18, show_
         plt.show()
     return iq, sync, filename
 
-
-# In[6]:
-
-
 def butterworth_filter(data, fs, cutoff_hz=200, order=1):
     b, a = butter(N=order, Wn=cutoff_hz, btype='low', fs=fs)
     zi = sp.signal.lfilter_zi(b, a)
     filt_data, _ = sp.signal.lfilter(b, a, data, zi=zi*data[0])
     return filt_data
-
-
-# In[ ]:
-
-
-
-
