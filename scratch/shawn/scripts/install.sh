3a01d3ce2c5b47b6102cdd2d4bf11f41671e883d
Larry Ruckman <ruckman@slac.stanford.edu>
2026-05-01 13:56:53 -0700

    refactor(scripts): Rename shawnhammer to smurf_startup and move to scripts/ (#85)
    Per issue #85, rename the shawnhammer startup script to smurf_startup
    and promote it from scratch/shawn/scripts/ to a new top-level scripts/
    directory. The other items from #85 (config-file driven, command-line
    flags, validation steps) were delivered earlier via #429, #433, #452,
    and #474.
    
    Symlinks at the old scratch paths preserve currently deployed systems
    that install via scratch/shawn/scripts/install.sh (which now points
    at the renamed files).

--- scratch/shawn/scripts/install.sh
+++ scratch/shawn/scripts/install.sh
@@ -1,8 +1,18 @@
 installdir=/usr/local/src/smurf-server-scripts/docker_scripts/
 
-for f in shawnhammer.sh ping_carrier.sh switch_carrier.sh shawnhammerfunctions.sh
+# smurf_startup{,_functions} live under the top-level scripts/ directory
+# (renamed from shawnhammer{,functions}.sh per issue #85).
+for f in ../../../scripts/smurf_startup.sh ../../../scripts/smurf_startup_functions.sh
+do
+    fbase=$(basename $f)
+    fstrip=${fbase%.*}
+    rm -f ${installdir}/$fstrip
+    ln -s `realpath $f` ${installdir}/$fstrip
+done
+
+for f in ping_carrier.sh switch_carrier.sh
 do
     fstrip=${f%.*}
-    rm ${installdir}/$fstrip
+    rm -f ${installdir}/$fstrip
     ln -s `pwd`/$f ${installdir}/$fstrip
 done
