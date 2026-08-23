af87bf1b02bc4e2adda9a4a9ebec8706f75b7f61
Shawn W. Henderson <20823858+swh76@users.noreply.github.com>
2026-08-22 19:18:43 -0700

    Modernize client Jupyter setup for Notebook v7+.

--- docker/client/jupyter/jupyter_config.py
+++ docker/client/jupyter/jupyter_config.py
@@ -1,11 +1,6 @@
 import os
-from notebook.auth import passwd
 
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = int(os.environ.get("JUPYTER_PORT", 8888))
-
-c.NotebookApp.allow_root = True
-c.NotebookApp.open_browser = False
-
-c.NotebookApp.password = passwd(os.environ.get("JUPYTER_PW", 'password'))
-c.NotebookApp.password_required = True
+c.ServerApp.ip = '0.0.0.0'
+c.ServerApp.port = int(os.environ.get("JUPYTER_PORT", 8888))
+c.ServerApp.allow_root = True
+c.ServerApp.open_browser = False
