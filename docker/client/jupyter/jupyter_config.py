33fe681970768f82c8fd223a11c5e5785ad6e3da
Shawn W. Henderson <20823858+swh76@users.noreply.github.com>
2026-08-22 19:33:35 -0700

    Minor fixes.

--- docker/client/jupyter/jupyter_config.py
+++ docker/client/jupyter/jupyter_config.py
@@ -4,3 +4,5 @@ c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.port = int(os.environ.get("JUPYTER_PORT", 8888))
 c.ServerApp.allow_root = True
 c.ServerApp.open_browser = False
+c.ServerApp.websocket_ping_interval = 90000
+c.ServerApp.websocket_ping_timeout = 30000
